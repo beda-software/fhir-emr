235a2de730d956a16e760829fedc66e31a3e5c4e
atuonufure <atuonufure@gmail.com>
2021-12-17 20:51:43 +0300

    Add patient encounter list

---
+++ web/src/components/PatientEncounter/index.tsx
@@ -0,0 +1,61 @@
+import { Table } from 'antd';
+import { useHistory } from 'react-router-dom';
+
+import { RenderRemoteData } from 'aidbox-react/lib/components/RenderRemoteData';
+
+import { useEncounterList } from 'src/containers/EncounterList/hooks';
+
+interface Props {
+    patientId: string;
+}
+
+const columns = [
+    {
+        title: 'Врач',
+        dataIndex: 'practitioner',
+        key: 'practitioner',
+    },
+    {
+        title: 'Статус',
+        dataIndex: 'status',
+        key: 'status',
+    },
+    {
+        title: 'Дата приема',
+        dataIndex: 'date',
+        key: 'date',
+    },
+];
+
+export const PatientEncounter = ({ patientId }: Props) => {
+    const history = useHistory();
+    const encounterDataListRD = useEncounterList({ subject: patientId });
+
+    return (
+        <div style={infoContainerStyle}>
+            <RenderRemoteData remoteData={encounterDataListRD}>
+                {(tableData) => (
+                    <Table
+                        dataSource={tableData}
+                        columns={columns}
+                        onRow={(record, rowIndex) => {
+                            return {
+                                onClick: (event) => {
+                                    history.push(`/encounters/${record.key}`);
+                                },
+                            };
+                        }}
+                    />
+                )}
+            </RenderRemoteData>
+        </div>
+    );
+};
+
+const infoContainerStyle = {
+    width: 1080,
+    backgroundColor: '#ffffff',
+    padding: '32px 40px',
+    boxShadow: '0px 6px 16px #E6EBF5',
+    marginTop: 54,
+};
