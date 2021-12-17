d58d5e704488fedbf2719236080ada37eb1b1367
Vadim Laletin <rusmiligamer@gmail.com>
2021-12-17 17:46:27 +0300

    Add an ability to move items in questionnaire builder

--- web/src/containers/QuestionnaireBuilder/index.tsx
+++ web/src/containers/QuestionnaireBuilder/index.tsx
@@ -1,11 +1,12 @@
-import { Form, FormInstance, Input } from 'antd';
+import { Button, Form, FormInstance, Input } from 'antd';
+import isEqual from 'lodash/isEqual';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import { uuid4 } from 'aidbox-react/lib/utils/uuid';
 
 import { Questionnaire, QuestionnaireItem } from 'shared/src/contrib/aidbox';
-import { getByPath, setByPath } from 'shared/src/utils/path';
+import { getByPath, setByPath, unsetByPath } from 'shared/src/utils/path';
 
 import { BaseLayout } from 'src/components/BaseLayout';
 
@@ -13,6 +14,21 @@ const ItemTypes = {
     GROUP: 'group',
     PRIMITIVE: 'primitive',
 };
+
+type NewDraggableItem = {
+    item: Omit<QuestionnaireItem, 'linkId'> & Partial<Pick<QuestionnaireItem, 'linkId'>>;
+    type: 'new';
+};
+type ExistingDraggableItem = {
+    item: QuestionnaireItem;
+    type: 'existing';
+    path: Array<string | number>;
+};
+type DraggableItem = NewDraggableItem | ExistingDraggableItem;
+
+function isNewDraggableItem(item: DraggableItem): item is NewDraggableItem {
+    return item.type === 'new';
+}
 
 export function QuestionnaireBuilder() {
     return (
@@ -46,15 +62,32 @@ function DroppableQuestionnaire() {
     const [form] = Form.useForm<Questionnaire>();
 
     return (
-        <Form<Questionnaire> form={form} initialValues={questionnaire}>
+        <Form<Questionnaire>
+            form={form}
+            initialValues={questionnaire}
+            onFinish={(values) => console.log('final', { ...questionnaire, ...values })}
+        >
             {() => {
                 const formValues = form.getFieldsValue();
                 console.log('value', formValues);
 
                 return (
-                    <div
-                    >
-                        <QuestionnaireItems items={formValues.item} parentPath={[]} form={form} />
+                    <div>
+                        <Form.Item>
+                            <Button htmlType="submit" type="primary">
+                                Сохранить
+                            </Button>
+                        </Form.Item>
+                        <QuestionnaireItemComponents
+                            items={formValues.item}
+                            parentPath={[]}
+                            form={form}
+                        />
+                        <Form.Item>
+                            <Button htmlType="submit" type="primary">
+                                Сохранить
+                            </Button>
+                        </Form.Item>
                     </div>
                 );
             }}
@@ -62,7 +95,7 @@ function DroppableQuestionnaire() {
     );
 }
 
-function QuestionnaireItems({
+function QuestionnaireItemComponents({
     items,
     parentPath,
     form,
@@ -71,7 +104,7 @@ function QuestionnaireItems({
     parentPath: Array<string | number>;
     form: FormInstance;
 }) {
-    const [{ isOverCurrent }, drop] = useDrop<Pick<QuestionnaireItem, 'type'>, any, any>(() => ({
+    const [{ isOverCurrent }, drop] = useDrop<DraggableItem, any, any>(() => ({
         accept: [ItemTypes.GROUP, ItemTypes.PRIMITIVE],
         drop: (item, monitor) => {
             const didDrop = monitor.didDrop();
@@ -81,22 +114,44 @@ function QuestionnaireItems({
             }
 
             const values = form.getFieldsValue();
-            form.setFieldsValue(
-                setByPath(
-                    values,
-                    [...parentPath, 'item'],
-                    [
-                        ...getByPath(values, [...parentPath, 'item'], []),
-                        { ...item, linkId: uuid4() },
-                    ],
-                ),
-            );
+            if (isNewDraggableItem(item)) {
+                form.setFieldsValue(
+                    setByPath(
+                        values,
+                        [...parentPath, 'item'],
+                        [
+                            ...getByPath(values, [...parentPath, 'item'], []),
+                            { linkId: uuid4(), ...item.item },
+                        ],
+                    ),
+                );
+            } else {
+                form.setFieldsValue(
+                    unsetByPath(
+                        setByPath(
+                            values,
+                            [...parentPath, 'item'],
+                            [...getByPath(values, [...parentPath, 'item'], []), item.item],
+                        ),
+                        item.path,
+                    ),
+                );
+            }
+        },
+        canDrop: (item) => {
+            if (!isNewDraggableItem(item)) {
+                if (isEqual(item.path, parentPath)) {
+                    return false;
+                }
+            }
+
+            return true;
         },
         collect: (monitor) => ({
-            isOverCurrent: monitor.isOver({ shallow: true }),
+            isOverCurrent: monitor.isOver({ shallow: true }) && monitor.canDrop(),
         }),
     }));
-    const backgroundColor = isOverCurrent ? 'darkgreen' : 'inherit';
+    const backgroundColor = isOverCurrent ? 'darkgreen' : 'white';
 
     return (
         <Form.Item name={[...parentPath, 'item']}>
@@ -111,40 +166,79 @@ function QuestionnaireItems({
                     backgroundColor,
                 }}
             >
-                {items?.map((qItem, index) => (
-                    <div key={qItem.linkId} style={{ marginLeft: 48, marginRight: 48 }}>
-                        {qItem.linkId} {qItem.text} {qItem.type}
-                        <QuestionnaireItems
-                            items={qItem.item}
-                            parentPath={[...parentPath, 'item', index]}
+                {items?.map((qItem, index) => {
+                    if (!qItem.linkId) {
+                        return null;
+                    }
+
+                    return (
+                        <QuestionnaireItemComponent
+                            item={qItem}
+                            key={qItem.linkId}
+                            index={index}
+                            parentPath={parentPath}
                             form={form}
                         />
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </Form.Item>
     );
 }
 
+function QuestionnaireItemComponent({
+    item,
+    parentPath,
+    form,
+    index,
+}: {
+    item: QuestionnaireItem;
+    parentPath: Array<string | number>;
+    form: FormInstance;
+    index: number;
+}) {
+    const [{ isDragging }, drag] = useDrag<ExistingDraggableItem, any, any>(
+        () => ({
+            type: item.type === 'group' ? ItemTypes.GROUP : ItemTypes.PRIMITIVE,
+            collect: (monitor) => ({
+                isDragging: !!monitor.isDragging(),
+            }),
+            item: { type: 'existing', item, path: [...parentPath, 'item', index] },
+        }),
+        [item, parentPath, index],
+    );
+
+    return (
+        <div ref={drag} key={item.linkId} style={{ marginLeft: 48, marginRight: 48 }}>
+            {item.linkId} {item.text} {item.type}
+            <QuestionnaireItemComponents
+                items={item.item}
+                parentPath={[...parentPath, 'item', index]}
+                form={form}
+            />
+        </div>
+    );
+}
+
 function GroupItemTemplate() {
-    const [{ isDragging }, drag] = useDrag(() => ({
+    const [{ isDragging }, drag] = useDrag<NewDraggableItem, any, any>(() => ({
         type: ItemTypes.GROUP,
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging(),
         }),
-        item: { type: 'group' },
+        item: { type: 'new', item: { type: 'group' } },
     }));
 
     return <div ref={drag}>Group</div>;
 }
 
 function PrimitiveComponentTemplate() {
-    const [{ isDragging }, drag] = useDrag(() => ({
+    const [{ isDragging }, drag] = useDrag<NewDraggableItem, any, any>(() => ({
         type: ItemTypes.PRIMITIVE,
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging(),
         }),
-        item: { type: 'string' },
+        item: { type: 'new', item: { type: 'text' } },
     }));
 
     return <div ref={drag}>Primitive</div>;
