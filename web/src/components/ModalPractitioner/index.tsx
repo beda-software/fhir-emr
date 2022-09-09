7c92ecf6f1ea7a8e35fdb9ea92f88a8b3fcf5d40
Aleksandr Pavlushkin <pavlushkin87@gmail.com>
2022-09-09 19:33:41 +0200

    Update seeds

---
+++ web/src/components/ModalPractitioner/index.tsx
@@ -0,0 +1,69 @@
+import { t, Trans } from '@lingui/macro';
+import { Button, notification } from 'antd';
+import { BaseButtonProps } from 'antd/lib/button/button';
+
+import { Practitioner, PractitionerRole } from 'shared/src/contrib/aidbox';
+import { questionnaireIdLoader } from 'shared/src/hooks/questionnaire-response-form-data';
+
+import { ModalTrigger } from '../ModalTrigger';
+import { QuestionnaireResponseForm } from '../QuestionnaireResponseForm';
+
+interface Props {
+    modalTitle: string;
+    buttonText: string;
+    icon: React.ReactNode;
+    buttonType: BaseButtonProps['type'];
+    questionnaireId: string;
+    practitionerResource?: Practitioner;
+    practitionerRole?: PractitionerRole;
+    practitionerListReload: () => void;
+}
+
+export function ModalNewPractitioner(props: Props) {
+    const {
+        buttonText,
+        icon,
+        buttonType,
+        questionnaireId,
+        practitionerResource,
+        modalTitle,
+        practitionerRole,
+        practitionerListReload,
+    } = props;
+
+    const launchContextParameters = practitionerResource
+        ? [
+              {
+                  name: 'Practitioner',
+                  resource: practitionerResource,
+              },
+              {
+                  name: 'PractitionerRole',
+                  resource: practitionerRole,
+              },
+          ]
+        : undefined;
+
+    return (
+        <ModalTrigger
+            title={modalTitle}
+            trigger={
+                <Button icon={icon} type={buttonType}>
+                    <Trans>{buttonText}</Trans>
+                </Button>
+            }
+        >
+            {({ closeModal }) => (
+                <QuestionnaireResponseForm
+                    questionnaireLoader={questionnaireIdLoader(questionnaireId)}
+                    launchContextParameters={launchContextParameters}
+                    onSuccess={() => {
+                        practitionerListReload();
+                        closeModal();
+                        notification.success({ message: t`Practitioner successfully created` });
+                    }}
+                />
+            )}
+        </ModalTrigger>
+    );
+}
