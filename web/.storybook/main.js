ec464c648006f249196fd2490b5e5d7143a18490
Ilya Beda <ir4y.ix@gmail.com>
2022-10-20 11:28:00 +0400

    Fix storybook

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-preset-craco'],
+    addons: [
+       '@storybook/addon-links',
+       '@storybook/addon-essentials',
+       '@storybook/preset-scss',
+    ],
     core: {
         builder: 'webpack5',
     },
