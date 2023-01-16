5fe2f049b558b788e4787e699bb3b5dc074c9916
Elena Grednikova <vesnushkalena@gmail.com>
2023-01-16 19:39:31 +0100

    Fix documents list layout

--- web/src/containers/PatientDetails/PatientDocument/usePatientDocument.ts
+++ web/src/containers/PatientDetails/PatientDocument/usePatientDocument.ts
@@ -1,28 +1,27 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { getReference, WithId } from 'aidbox-react/lib/services/fhir';
 
-import { Patient } from 'shared/src/contrib/aidbox';
+import { Patient, QuestionnaireResponse } from 'shared/src/contrib/aidbox';
 import { questionnaireIdLoader } from 'shared/src/hooks/questionnaire-response-form-data';
 
 import { useQuestionnaireResponseForm } from 'src/components/QuestionnaireResponseForm';
 
 export interface PatientDocumentProps {
     patient: WithId<Patient>;
+    questionnaireResponse?: WithId<QuestionnaireResponse>;
 }
 
-export function usePatientDocument(props: PatientDocumentProps) {
-    const { patient } = props;
-    const params = useParams<{ questionnaireId: string }>();
-    const questionnaireId = params.questionnaireId!;
+export function usePatientDocument(props: PatientDocumentProps & { questionnaireId: string }) {
+    const { patient, questionnaireResponse, questionnaireId } = props;
     const navigate = useNavigate();
 
     const data = useQuestionnaireResponseForm({
         questionnaireLoader: questionnaireIdLoader(questionnaireId),
         launchContextParameters: [{ name: 'Patient', resource: patient }],
-        initialQuestionnaireResponse: {
+        initialQuestionnaireResponse: questionnaireResponse || {
             source: getReference(patient),
-            questionnaire: params.questionnaireId!,
+            questionnaire: questionnaireId,
         },
         onSuccess: () => navigate(`/patients/${patient.id}/documents`),
     });
