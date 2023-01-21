d4337cf15130cb659e9a1e8fd83a507d35db1d1f
Dmitry Shutov <dmitryashutov@gmail.com>
2023-01-21 14:36:51 +0600

    Fix QuesionBoolean fieldName path

--- web/src/components/BaseQuestionnaireResponseForm/widgets/boolean.tsx
+++ web/src/components/BaseQuestionnaireResponseForm/widgets/boolean.tsx
@@ -4,13 +4,11 @@ import { QuestionItemProps, useQuestionnaireResponseFormContext } from 'sdc-qrf'
 export function QuestionBoolean({ parentPath, questionItem }: QuestionItemProps) {
     const qrfContext = useQuestionnaireResponseFormContext();
     const { linkId, text, readOnly, hidden } = questionItem;
-    const fieldName = [...parentPath, linkId, 0, 'value', 'integer'];
+    const fieldName = [...parentPath, linkId, 0, 'value', 'boolean'];
 
     return (
-        <Form.Item label={text} name={fieldName} hidden={hidden}>
-            <Checkbox
-                disabled={readOnly || qrfContext.readOnly}
-            />
+        <Form.Item label={text} name={fieldName} hidden={hidden} valuePropName="checked">
+            <Checkbox disabled={readOnly || qrfContext.readOnly} />
         </Form.Item>
     );
 }
