e9a2f562f8d9ea9bdf26e923149db5596835fb08
@MikhailNikiforov_beda.software <michail@beda.software>
2023-02-08 17:21:54 +0100

    Fix ssn prop saving to the patient resource
    Add test for patient create questionnaire
    
    Ref #54

---
+++ web/src/components/ModalNewPatient/__test__/create-patient.test.tsx
@@ -0,0 +1,115 @@
+import { act, renderHook } from '@testing-library/react';
+import { getFHIRResource, getFHIRResources } from 'aidbox-react/lib/services/fhir';
+import { ensure } from 'aidbox-react/lib/utils/tests';
+
+import { Patient, Questionnaire } from 'shared/src/contrib/aidbox';
+import {
+    questionnaireIdLoader,
+    QuestionnaireResponseFormData,
+} from 'shared/src/hooks/questionnaire-response-form-data';
+
+import { useQuestionnaireResponseForm } from 'src/components/QuestionnaireResponseForm';
+import { loginAdminUser } from 'src/setupTests';
+
+describe('createPatient', () => {
+    beforeAll(async () => {
+        await loginAdminUser();
+    });
+
+    test('should create patient', async () => {
+        const createPatientQR = ensure(
+            await getFHIRResource<Questionnaire>({
+                resourceType: 'Questionnaire',
+                id: 'patient-create',
+            }),
+        );
+
+        let patientsBundle = ensure(await getFHIRResources<Patient>('Patient', {}));
+        expect(patientsBundle.total).toBe(0);
+
+        const formData: QuestionnaireResponseFormData = {
+            context: {
+                questionnaire: createPatientQR,
+                questionnaireResponse: {
+                    resourceType: 'QuestionnaireResponse',
+                    questionnaire: undefined,
+                    status: 'in-progress',
+                },
+                launchContextParameters: [],
+            },
+            formValues: {
+                'patient-id': [
+                    {
+                        value: {},
+                    },
+                ],
+                'last-name': [
+                    {
+                        value: {
+                            string: 'Doe',
+                        },
+                    },
+                ],
+                'first-name': [
+                    {
+                        value: {
+                            string: 'John',
+                        },
+                    },
+                ],
+                'birth-date': [
+                    {
+                        value: {
+                            date: '2000-02-01',
+                        },
+                    },
+                ],
+                gender: [
+                    {
+                        value: {
+                            string: 'male',
+                        },
+                    },
+                ],
+                ssn: [
+                    {
+                        value: {
+                            string: '123123123',
+                        },
+                    },
+                ],
+                mobile: [
+                    {
+                        value: {
+                            string: '11231231231',
+                        },
+                    },
+                ],
+            },
+        };
+
+        const { result } = renderHook(() =>
+            useQuestionnaireResponseForm({
+                questionnaireLoader: questionnaireIdLoader('patient-create'),
+                launchContextParameters: [],
+            }),
+        );
+
+        await act(async () => {
+            await result.current.onSubmit(formData);
+        });
+
+        patientsBundle = ensure(await getFHIRResources<Patient>('Patient', {}));
+        expect(patientsBundle.total).toBe(1);
+
+        const patient = patientsBundle!.entry![0]!.resource!;
+
+        expect(patient.id).toBeDefined();
+        expect(patient.name![0]!.family).toBe('Doe');
+        expect(patient.name![0]!.given![0]).toBe('John');
+        expect(patient.gender).toBe('male');
+        expect(patient.birthDate).toBe('2000-02-01');
+        expect(patient.identifier![0]!.value).toBe('123123123');
+        expect(patient.telecom![0]!.value).toBe('11231231231');
+    });
+});
