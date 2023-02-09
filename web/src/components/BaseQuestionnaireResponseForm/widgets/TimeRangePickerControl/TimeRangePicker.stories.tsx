1dc366607e59ad5c4e8d4e0f6e3f34fbfb1333e2
@MikhailNikiforov_beda.software <michail@beda.software>
2023-02-09 10:41:22 +0100

    Add range time picker to storybook
    Ref #40

---
+++ web/src/components/BaseQuestionnaireResponseForm/widgets/TimeRangePickerControl/TimeRangePicker.stories.tsx
@@ -0,0 +1,49 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoryQuestionDecorator } from 'src/components/BaseQuestionnaireResponseForm/widgets/utils-stories';
+
+import { TimeRangePickerControl } from './index';
+
+export default {
+    title: 'widget/TimeRangePicker',
+    component: TimeRangePickerControl,
+    parameters: {
+        storyshots: { disable: true },
+    },
+    decorators: [
+        (Story) => (
+            <StoryQuestionDecorator>
+                <Story />
+            </StoryQuestionDecorator>
+        ),
+    ],
+} as ComponentMeta<typeof TimeRangePickerControl>;
+
+const Template: ComponentStory<typeof TimeRangePickerControl> = (args) => (
+    <TimeRangePickerControl {...args} />
+);
+
+export const CorrectItemsCount = Template.bind({});
+CorrectItemsCount.args = {
+    parentPath: [],
+    questionItem: {
+        text: 'Time range picker',
+        type: 'time-range-picker',
+        linkId: 'example',
+        required: true,
+        item: [
+            { type: 'time', linkId: 'start' },
+            { type: 'time', linkId: 'end' },
+        ],
+    },
+};
+
+export const NoItems = Template.bind({});
+NoItems.args = {
+    parentPath: [],
+    questionItem: {
+        text: 'No items',
+        type: 'time-range-picker',
+        linkId: 'example',
+        required: true,
+    },
+};
