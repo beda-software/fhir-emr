cf54c344dc3194ddcf2f0b10a8a747f61b34ee08
atuonufure <atuonufure@gmail.com>
2023-02-10 12:31:38 +0100

    Add reading authorized practitioner name in app header
    Ref: #53

--- web/src/components/BaseLayout/Header/index.tsx
+++ web/src/components/BaseLayout/Header/index.tsx
@@ -19,6 +19,7 @@ import { LogoImage } from 'src/images/LogoImage';
 import { logout } from 'src/services/auth';
 
 import s from './Header.module.scss';
+import { sharedAuthorisedPractitioner } from 'src/sharedState';
 
 export interface RouteItem {
     path: string;
@@ -89,6 +90,9 @@ export function AppHeader() {
             }
         };
 
+        const practitionerData = sharedAuthorisedPractitioner.getSharedState();
+        const practitionerName = practitionerData?.name?.[0];
+
         return (
             <Dropdown
                 menu={{ items: userMenu, onClick: onUserMenuClick }}
@@ -98,7 +102,7 @@ export function AppHeader() {
             >
                 <a onClick={(e) => e.preventDefault()} className={s.user}>
                     <AvatarImage className={s.avatar} />
-                    <span>{renderHumanName()}</span>
+                    <span>{renderHumanName(practitionerName)}</span>
                     <DownOutlined className={s.localeArrow} />
                 </a>
             </Dropdown>
