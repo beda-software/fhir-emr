e83b6712d644de3d0d52f28921e6c8a3bd4b5df0
atuonufure <atuonufure@gmail.com>
2023-02-23 08:43:20 +0100

    Add usePager to patients list
    Ref: #61

--- web/src/containers/PatientList/hooks.ts
+++ web/src/containers/PatientList/hooks.ts
@@ -1,5 +1,9 @@
-import { useService } from 'aidbox-react/lib/hooks/service';
-import { extractBundleResources, getFHIRResources } from 'aidbox-react/lib/services/fhir';
+import { TablePaginationConfig } from 'antd';
+import { useEffect, useState } from 'react';
+
+import { usePager } from 'aidbox-react/lib/hooks/pager';
+import { isSuccess } from 'aidbox-react/lib/libs/remoteData';
+import { extractBundleResources } from 'aidbox-react/lib/services/fhir';
 import { mapSuccess } from 'aidbox-react/lib/services/service';
 
 import { Patient } from 'shared/src/contrib/aidbox';
@@ -10,16 +14,46 @@ import { useDebounce } from 'src/utils/debounce';
 export function usePatientList(filterValues: StringTypeColumnFilterValue[]) {
     const debouncedFilterValues = useDebounce(filterValues, 300);
 
-    const [patientsResponse, patientsResponseManager] = useService(async () => {
-        const patientFilterValue = debouncedFilterValues[0];
+    const [pageSize, setPageSize] = useState(10);
 
-        return mapSuccess(
-            await getFHIRResources<Patient>('Patient', {
-                ...(patientFilterValue ? { name: patientFilterValue.value } : {}),
-            }),
-            (bundle) => extractBundleResources(bundle).Patient,
-        );
+    const patientFilterValue = debouncedFilterValues[0];
+
+    const [resourceResponse, pagerManager] = usePager<Patient>('Patient', pageSize, {
+        _sort: '-_lastUpdated',
+        ...(patientFilterValue ? { name: patientFilterValue.value } : {}),
+    });
+
+    const patientsResponse = mapSuccess(
+        resourceResponse,
+        (bundle) => extractBundleResources(bundle).Patient,
+    );
+
+    const pagination = {
+        current: pagerManager.currentPage,
+        pageSize: pageSize,
+        total: isSuccess(resourceResponse) ? resourceResponse.data.total : 0,
+    };
+
+    const handleTableChange = async (pagination: TablePaginationConfig) => {
+        if (typeof pagination.current !== 'number') return;
+
+        if (pagination.pageSize && pagination.pageSize !== pageSize) {
+            pagerManager.loadPage(pagination.current);
+            setPageSize(pagination.pageSize);
+        } else {
+            pagerManager.loadPage(pagination.current);
+        }
+    };
+
+    useEffect(() => {
+        pagerManager.reload();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [debouncedFilterValues]);
 
-    return { patientsResponse, patientsResponseManager };
+    return {
+        pagination,
+        patientsResponse,
+        pagerManager,
+        handleTableChange,
+    };
 }
