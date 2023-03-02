9ad2a15c00282b6be5ff4b5dbe359f9aa65fbd0b
atuonufure <atuonufure@gmail.com>
2023-03-02 04:31:54 +0100

    Add usePager to questionnaires
    Ref: #61

--- web/src/containers/QuestionnaireList/hooks.ts
+++ web/src/containers/QuestionnaireList/hooks.ts
@@ -1,5 +1,9 @@
-import { useService } from 'aidbox-react/lib/hooks/service';
-import { extractBundleResources, getFHIRResources } from 'aidbox-react/lib/services/fhir';
+import { TablePaginationConfig } from 'antd';
+import { useEffect, useState } from 'react';
+
+import { usePager } from 'aidbox-react/lib/hooks/pager';
+import { isSuccess } from 'aidbox-react/lib/libs/remoteData';
+import { extractBundleResources } from 'aidbox-react/lib/services/fhir';
 import { mapSuccess } from 'aidbox-react/lib/services/service';
 
 import { Questionnaire } from 'shared/src/contrib/aidbox';
@@ -10,16 +14,41 @@ import { useDebounce } from 'src/utils/debounce';
 export function useQuestionnaireList(filterValues: StringTypeColumnFilterValue[]) {
     const debouncedFilterValues = useDebounce(filterValues, 300);
 
-    const [questionnaireResponse, questionnaireResponseManager] = useService(async () => {
-        const questionnaireFilterValue = debouncedFilterValues[0];
+    const [pageSize, setPageSize] = useState(10);
 
-        return mapSuccess(
-            await getFHIRResources<Questionnaire>('Questionnaire', {
-                ...(questionnaireFilterValue ? { name: questionnaireFilterValue.value } : {}),
-            }),
-            (bundle) => extractBundleResources(bundle).Questionnaire,
-        );
+    const questionnaireFilterValue = debouncedFilterValues[0];
+
+    const [resourceResponse, pagerManager] = usePager<Questionnaire>('Questionnaire', pageSize, {
+        _sort: '-_lastUpdated',
+        ...(questionnaireFilterValue ? { name: questionnaireFilterValue.value } : {}),
+    });
+
+    const questionnaireListRD = mapSuccess(
+        resourceResponse,
+        (bundle) => extractBundleResources(bundle).Questionnaire,
+    );
+
+    const pagination = {
+        current: pagerManager.currentPage,
+        pageSize: pageSize,
+        total: isSuccess(resourceResponse) ? resourceResponse.data.total : 0,
+    };
+
+    const handleTableChange = async (pagination: TablePaginationConfig) => {
+        if (typeof pagination.current !== 'number') return;
+
+        if (pagination.pageSize && pagination.pageSize !== pageSize) {
+            pagerManager.loadPage(pagination.current);
+            setPageSize(pagination.pageSize);
+        } else {
+            pagerManager.loadPage(pagination.current);
+        }
+    };
+
+    useEffect(() => {
+        pagerManager.reload();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [debouncedFilterValues]);
 
-    return { questionnaireResponse, questionnaireResponseManager };
+    return { pagination, questionnaireListRD, pagerManager, handleTableChange };
 }
