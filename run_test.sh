4300e27b7a9fcb997527e6f4a812b232eb2aafe1
atuonufure <atuonufure@gmail.com>
2023-03-15 07:35:27 +0100

    Migrate test run to docker compose v2
    Ref: #88

--- run_test.sh
+++ run_test.sh
@@ -4,9 +4,8 @@ if [ -f ".env" ]; then
     export `cat .env`
 fi
 
-docker-compose -f docker-compose.tests.yaml pull
-docker-compose -f docker-compose.tests.yaml build
-docker-compose -f docker-compose.tests.yaml up --force-recreate -d
+docker compose -f docker-compose.tests.yaml pull
+docker compose -f docker-compose.tests.yaml up -d
 
 yarn test $@ --runInBand
 exit $?
