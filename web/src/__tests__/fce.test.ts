c0802f871a7a107a152427df149255cd64500028
Ilya Beda <ir4y.ix@gmail.com>
2023-03-27 14:46:55 +0400

    Add test for converter

---
+++ web/src/__tests__/fce.test.ts
@@ -0,0 +1,27 @@
+import { extractBundleResources, getFHIRResources } from 'aidbox-react/lib/services/fhir';
+import { service } from 'aidbox-react/lib/services/service';
+import { ensure } from 'aidbox-react/lib/utils/tests';
+import { Questionnaire as FHIRQuestionnaire } from 'fhir/r4b';
+import { Questionnaire as AidboxQuestionnaire } from 'shared/src/contrib/aidbox';
+import { loginAdminUser } from 'src/setupTests';
+import { toFirstClassExtension } from 'src/utils/fce';
+
+describe('Questionanire transformation', () => {
+    beforeAll(async () => {
+        await loginAdminUser();
+    });
+    test('Each Questionnaire shod converts', async () => {
+        const questionnaires = extractBundleResources(
+            ensure(await getFHIRResources<AidboxQuestionnaire>('Questionnaire', { _count: 9999 })),
+        ).Questionnaire;
+        for (let q of questionnaires) {
+            console.log('Convertion', q.id);
+            const fhirQuestionanire = ensure(
+                await service<FHIRQuestionnaire>({
+                    url: `/fhir/Questionnaire/${q.id}`,
+                }),
+            );
+            expect(toFirstClassExtension(fhirQuestionanire)).toStrictEqual(q);
+        }
+    });
+});
---
+++ web/src/utils/fce.ts
@@ -0,0 +1,14 @@
+import { Questionnaire as FHIRQuestionnaire } from 'fhir/r4b';
+import { Questionnaire as AidboxQuestionnaire } from 'shared/src/contrib/aidbox';
+
+export function toFirstClassExtension(q: FHIRQuestionnaire): AidboxQuestionnaire {
+    if (
+        !(
+            (q.meta?.profile?.length ?? 0) == 1 &&
+            q.meta?.profile?.[0] == 'https://beda.software/beda-emr-questionnaire'
+        )
+    ) {
+        throw new Error('Only beda emr questionanire supported');
+    }
+    return q as unknown as AidboxQuestionnaire;
+}
