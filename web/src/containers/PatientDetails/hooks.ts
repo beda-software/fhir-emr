e0e9d56443a861b2ad4ff1939e2355cbbf042697
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-04-17 11:45:42 +0200

    Do not propagate PatientDetails resource into global state

--- web/src/containers/PatientDetails/hooks.ts
+++ web/src/containers/PatientDetails/hooks.ts
@@ -1,23 +1,11 @@
 import { useService } from 'fhir-react/lib/hooks/service';
 import { getFHIRResource } from 'fhir-react/lib/services/fhir';
-import { mapSuccess } from 'fhir-react/lib/services/service';
 import { Patient } from 'fhir/r4b';
 
-import { sharedAuthorizedPatient } from 'src/sharedState';
-import { Role, selectCurrentUserRole } from 'src/utils/role';
-
 export function usePatientResource(config: { id: string }) {
-    return useService(async () => {
-        const patientResponse = await getFHIRResource<Patient>({
+    return useService(async () =>
+        getFHIRResource<Patient>({
             reference: `Patient/${config.id}`,
-        });
-        return mapSuccess(patientResponse, (patient) => {
-            selectCurrentUserRole({
-                [Role.Patient]: sharedAuthorizedPatient.setSharedState(patient),
-                [Role.Admin]: undefined,
-            });
-
-            return patient;
-        });
-    });
+        }),
+    );
 }
