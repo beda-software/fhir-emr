e92bf7ebd578e9d43001074b6dd19f0cd82c952a
atuonufure <atuonufure@gmail.com>
2023-04-25 09:52:08 +0200

    Add tests for the questionnaire converter identical to fhir-sdc

--- web/src/__tests__/fce.test.ts
+++ web/src/__tests__/fce.test.ts
@@ -13,6 +13,47 @@ import { toFirstClassExtension, fromFirstClassExtension } from 'shared/src/utils
 
 import { loginAdminUser } from 'src/setupTests';
 
+// fce
+import fce_allergies from './resources/questionnaire_fce/allergies.json';
+import fce_beverages from './resources/questionnaire_fce/beverages.json';
+import fce_choice_answer_option from './resources/questionnaire_fce/choice_answer_option.json';
+import fce_encounter_create from './resources/questionnaire_fce/encounter_create.json';
+import fce_gad_7 from './resources/questionnaire_fce/gad_7.json';
+import fce_immunization from './resources/questionnaire_fce/immunization.json';
+import fce_medication from './resources/questionnaire_fce/medication.json';
+import fce_multiple_type_launch_context from './resources/questionnaire_fce/multiple_type_launch_context.json';
+import fce_patient_create from './resources/questionnaire_fce/patient_create.json';
+import fce_patient_edit from './resources/questionnaire_fce/patient_edit.json';
+import fce_phq_2_phq_9 from './resources/questionnaire_fce/phq_2_phq_9.json';
+import fce_physicalexam from './resources/questionnaire_fce/physicalexam.json';
+import fce_practitioner_create from './resources/questionnaire_fce/practitioner_create.json';
+import fce_practitioner_edit from './resources/questionnaire_fce/practitioner_edit.json';
+import fce_practitioner_role_create from './resources/questionnaire_fce/practitioner_role_create.json';
+import fce_public_appointment from './resources/questionnaire_fce/public_appointment.json';
+import fce_review_of_systems from './resources/questionnaire_fce/review_of_systems.json';
+import fce_source_queries from './resources/questionnaire_fce/source_queries.json';
+import fce_vitals from './resources/questionnaire_fce/vitals.json';
+// fhir
+import fhir_allergies from './resources/questionnaire_fhir/allergies.json';
+import fhir_beverages from './resources/questionnaire_fhir/beverages.json';
+import fhir_choice_answer_option from './resources/questionnaire_fhir/choice_answer_option.json';
+import fhir_encounter_create from './resources/questionnaire_fhir/encounter_create.json';
+import fhir_gad_7 from './resources/questionnaire_fhir/gad_7.json';
+import fhir_immunization from './resources/questionnaire_fhir/immunization.json';
+import fhir_medication from './resources/questionnaire_fhir/medication.json';
+import fhir_multiple_type_launch_context from './resources/questionnaire_fhir/multiple_type_launch_context.json';
+import fhir_patient_create from './resources/questionnaire_fhir/patient_create.json';
+import fhir_patient_edit from './resources/questionnaire_fhir/patient_edit.json';
+import fhir_phq_2_phq_9 from './resources/questionnaire_fhir/phq_2_phq_9.json';
+import fhir_physicalexam from './resources/questionnaire_fhir/physicalexam.json';
+import fhir_practitioner_create from './resources/questionnaire_fhir/practitioner_create.json';
+import fhir_practitioner_edit from './resources/questionnaire_fhir/practitioner_edit.json';
+import fhir_practitioner_role_create from './resources/questionnaire_fhir/practitioner_role_create.json';
+import fhir_public_appointment from './resources/questionnaire_fhir/public_appointment.json';
+import fhir_review_of_systems from './resources/questionnaire_fhir/review_of_systems.json';
+import fhir_source_queries from './resources/questionnaire_fhir/source_queries.json';
+import fhir_vitals from './resources/questionnaire_fhir/vitals.json';
+
 const notWorkingQuestionnaires = [
     'edit-appointment',
     'encounter-create-from-appointment',
@@ -24,7 +65,7 @@ describe('Questionanire and QuestionnaireResponses transformation', () => {
     beforeAll(async () => {
         await loginAdminUser();
     });
-    test.skip('Each Questionnaires should convert to fce and back to fhir', async () => {
+    test.skip('Each Questionnaires should convert to fce and back to fhir (obsolete)', async () => {
         const questionnaires = extractBundleResources(
             ensure(await getFHIRResources<AidboxQuestionnaire>('Questionnaire', { _count: 9999 })),
         ).Questionnaire;
@@ -43,6 +84,62 @@ describe('Questionanire and QuestionnaireResponses transformation', () => {
             }
         }
     });
+    test('Each FHIR Questionnaire should convert to FCE', async () => {
+        expect(toFirstClassExtension(fhir_allergies as FHIRQuestionnaire)).toStrictEqual(fce_allergies);
+        expect(toFirstClassExtension(fhir_beverages as FHIRQuestionnaire)).toStrictEqual(fce_beverages);
+        expect(toFirstClassExtension(fhir_choice_answer_option as FHIRQuestionnaire)).toStrictEqual(
+            fce_choice_answer_option,
+        );
+        expect(toFirstClassExtension(fhir_encounter_create as FHIRQuestionnaire)).toStrictEqual(fce_encounter_create);
+        expect(toFirstClassExtension(fhir_gad_7 as FHIRQuestionnaire)).toStrictEqual(fce_gad_7);
+        expect(toFirstClassExtension(fhir_immunization as FHIRQuestionnaire)).toStrictEqual(fce_immunization);
+        expect(toFirstClassExtension(fhir_medication as FHIRQuestionnaire)).toStrictEqual(fce_medication);
+        expect(toFirstClassExtension(fhir_multiple_type_launch_context as FHIRQuestionnaire)).toStrictEqual(
+            fce_multiple_type_launch_context,
+        );
+        expect(toFirstClassExtension(fhir_patient_create as FHIRQuestionnaire)).toStrictEqual(fce_patient_create);
+        expect(toFirstClassExtension(fhir_patient_edit as FHIRQuestionnaire)).toStrictEqual(fce_patient_edit);
+        expect(toFirstClassExtension(fhir_phq_2_phq_9 as FHIRQuestionnaire)).toStrictEqual(fce_phq_2_phq_9);
+        expect(toFirstClassExtension(fhir_physicalexam as FHIRQuestionnaire)).toStrictEqual(fce_physicalexam);
+        expect(toFirstClassExtension(fhir_practitioner_create as FHIRQuestionnaire)).toStrictEqual(
+            fce_practitioner_create,
+        );
+        expect(toFirstClassExtension(fhir_practitioner_edit as FHIRQuestionnaire)).toStrictEqual(fce_practitioner_edit);
+        expect(toFirstClassExtension(fhir_practitioner_role_create as FHIRQuestionnaire)).toStrictEqual(
+            fce_practitioner_role_create,
+        );
+        expect(toFirstClassExtension(fhir_public_appointment as FHIRQuestionnaire)).toStrictEqual(
+            fce_public_appointment,
+        );
+        expect(toFirstClassExtension(fhir_review_of_systems as FHIRQuestionnaire)).toStrictEqual(fce_review_of_systems);
+        expect(toFirstClassExtension(fhir_source_queries as FHIRQuestionnaire)).toStrictEqual(fce_source_queries);
+        expect(toFirstClassExtension(fhir_vitals as FHIRQuestionnaire)).toStrictEqual(fce_vitals);
+    });
+    test('Each FCE Questionnaire should convert to FHIR', async () => {
+        expect(fromFirstClassExtension(fce_allergies as any)).toStrictEqual(fhir_allergies);
+        expect(fromFirstClassExtension(fce_beverages as any)).toStrictEqual(fhir_beverages);
+        expect(fromFirstClassExtension(fce_choice_answer_option as any)).toStrictEqual(fhir_choice_answer_option);
+        expect(fromFirstClassExtension(fce_encounter_create as any)).toStrictEqual(fhir_encounter_create);
+        expect(fromFirstClassExtension(fce_gad_7 as any)).toStrictEqual(fhir_gad_7);
+        expect(fromFirstClassExtension(fce_immunization as any)).toStrictEqual(fhir_immunization);
+        expect(fromFirstClassExtension(fce_medication as any)).toStrictEqual(fhir_medication);
+        expect(fromFirstClassExtension(fce_multiple_type_launch_context as any)).toStrictEqual(
+            fhir_multiple_type_launch_context,
+        );
+        expect(fromFirstClassExtension(fce_patient_create as any)).toStrictEqual(fhir_patient_create);
+        expect(fromFirstClassExtension(fce_patient_edit as any)).toStrictEqual(fhir_patient_edit);
+        expect(fromFirstClassExtension(fce_phq_2_phq_9 as any)).toStrictEqual(fhir_phq_2_phq_9);
+        expect(fromFirstClassExtension(fce_physicalexam as any)).toStrictEqual(fhir_physicalexam);
+        expect(fromFirstClassExtension(fce_practitioner_create as any)).toStrictEqual(fhir_practitioner_create);
+        expect(fromFirstClassExtension(fce_practitioner_edit as any)).toStrictEqual(fhir_practitioner_edit);
+        expect(fromFirstClassExtension(fce_practitioner_role_create as any)).toStrictEqual(
+            fhir_practitioner_role_create,
+        );
+        expect(fromFirstClassExtension(fce_public_appointment as any)).toStrictEqual(fhir_public_appointment);
+        expect(fromFirstClassExtension(fce_review_of_systems as any)).toStrictEqual(fhir_review_of_systems);
+        expect(fromFirstClassExtension(fce_source_queries as any)).toStrictEqual(fhir_source_queries);
+        expect(fromFirstClassExtension(fce_vitals as any)).toStrictEqual(fhir_vitals);
+    });
     test('Each FHIR QuestionnaireResponse should convert to FCE', async () => {
         expect(toFirstClassExtension(practitioner_fhir_QuestionnaireResponse)).toStrictEqual(
             practitioner_aidbox_QuestionnaireResponse,
@@ -53,9 +150,7 @@ describe('Questionanire and QuestionnaireResponses transformation', () => {
         expect(toFirstClassExtension(allergies_fhir_QuestionnaireResponse)).toStrictEqual(
             allergies_aidbox_QuestionnaireResponse,
         );
-        expect(toFirstClassExtension(gad7_fhir_QuestionnaireResponse)).toStrictEqual(
-            gad7_aidbox_QuestionnaireResponse,
-        );
+        expect(toFirstClassExtension(gad7_fhir_QuestionnaireResponse)).toStrictEqual(gad7_aidbox_QuestionnaireResponse);
         expect(toFirstClassExtension(medication_fhir_QuestionnaireResponse)).toStrictEqual(
             medication_aidbox_QuestionnaireResponse,
         );
@@ -77,9 +172,9 @@ describe('Questionanire and QuestionnaireResponses transformation', () => {
         expect(toFirstClassExtension(cardiology_fhir_QuestionnaireResponse)).toStrictEqual(
             cardiology_aidbox_QuestionnaireResponse,
         );
-        expect(
-            toFirstClassExtension(allergies_inprogress_fhir_QuestionnaireResponse),
-        ).toStrictEqual(allergies_inprogress_aidbox_QuestionnaireResponse);
+        expect(toFirstClassExtension(allergies_inprogress_fhir_QuestionnaireResponse)).toStrictEqual(
+            allergies_inprogress_aidbox_QuestionnaireResponse,
+        );
         expect(toFirstClassExtension(newappointment_fhir_QuestionnaireResponse)).toStrictEqual(
             newappointment_aidbox_QuestionnaireResponse,
         );
@@ -118,9 +213,9 @@ describe('Questionanire and QuestionnaireResponses transformation', () => {
         expect(fromFirstClassExtension(cardiology_aidbox_QuestionnaireResponse)).toStrictEqual(
             cardiology_fhir_QuestionnaireResponse,
         );
-        expect(
-            fromFirstClassExtension(allergies_inprogress_aidbox_QuestionnaireResponse),
-        ).toStrictEqual(allergies_inprogress_fhir_QuestionnaireResponse);
+        expect(fromFirstClassExtension(allergies_inprogress_aidbox_QuestionnaireResponse)).toStrictEqual(
+            allergies_inprogress_fhir_QuestionnaireResponse,
+        );
         expect(fromFirstClassExtension(newappointment_aidbox_QuestionnaireResponse)).toStrictEqual(
             newappointment_fhir_QuestionnaireResponse,
         );
@@ -1191,8 +1286,7 @@ const physicalexam_fhir_QuestionnaireResponse: FHIRQuestionnaireResponse = JSON.
                     {
                         answer: [
                             {
-                                valueString:
-                                    'Supple, normal ROM, no lymphadenopathy/masses, nontender',
+                                valueString: 'Supple, normal ROM, no lymphadenopathy/masses, nontender',
                             },
                         ],
                         linkId: 'neck',
@@ -1208,8 +1302,7 @@ const physicalexam_fhir_QuestionnaireResponse: FHIRQuestionnaireResponse = JSON.
                     {
                         answer: [
                             {
-                                valueString:
-                                    'No respiratory distress, lungs CTAB: no rales, rhonchi, or wheeze',
+                                valueString: 'No respiratory distress, lungs CTAB: no rales, rhonchi, or wheeze',
                             },
                         ],
                         linkId: 'pulmonary',
@@ -1252,8 +1345,7 @@ const physicalexam_fhir_QuestionnaireResponse: FHIRQuestionnaireResponse = JSON.
                     {
                         answer: [
                             {
-                                valueString:
-                                    'Normal mood and affect. Judgement/competence is appropriate',
+                                valueString: 'Normal mood and affect. Judgement/competence is appropriate',
                             },
                         ],
                         linkId: 'psychiatric',
@@ -1261,8 +1353,7 @@ const physicalexam_fhir_QuestionnaireResponse: FHIRQuestionnaireResponse = JSON.
                     {
                         answer: [
                             {
-                                valueString:
-                                    'Warm, dry, and intact. No rashes, dermatoses, petechiae, or lesions',
+                                valueString: 'Warm, dry, and intact. No rashes, dermatoses, petechiae, or lesions',
                             },
                         ],
                         linkId: 'skin',
@@ -1270,8 +1361,7 @@ const physicalexam_fhir_QuestionnaireResponse: FHIRQuestionnaireResponse = JSON.
                     {
                         answer: [
                             {
-                                valueString:
-                                    'Normal sensation bilaterally on soles of feet with 10g monofilament',
+                                valueString: 'Normal sensation bilaterally on soles of feet with 10g monofilament',
                             },
                         ],
                         linkId: 'monofilament',
@@ -1288,8 +1378,7 @@ const physicalexam_fhir_QuestionnaireResponse: FHIRQuestionnaireResponse = JSON.
                     {
                         answer: [
                             {
-                                valueString:
-                                    'External genitalia without erythema, exudate or discharge',
+                                valueString: 'External genitalia without erythema, exudate or discharge',
                             },
                         ],
                         linkId: 'genitourinary-female',
