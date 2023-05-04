11beaee8b031f2d7b4e4727441702ec52199ce51
atuonufure <atuonufure@gmail.com>
2023-05-04 04:29:06 +0200

    Fix unit in readonly widgets

--- web/src/components/BaseQuestionnaireResponseForm/readonly-widgets/integer.tsx
+++ web/src/components/BaseQuestionnaireResponseForm/readonly-widgets/integer.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
 import { QuestionItemProps } from 'sdc-qrf';
 
+import { Coding } from 'shared/src/contrib/aidbox';
+
 import { useFieldController } from 'src/components/BaseQuestionnaireResponseForm/hooks';
 import { formatUnit } from 'src/utils/unit';
 
@@ -15,12 +17,12 @@ export function QuestionInteger({ parentPath, questionItem }: QuestionItemProps)
         return null;
     }
 
-    const { unit } = questionItem as { unit?: string };
+    const { unit } = questionItem as { unit?: Coding };
 
     return (
         <p className={classNames(s.question, s.row, 'form__question')}>
             <span className={s.questionText}>{text}</span>
-            <span className={s.answer}>{value ? `${value} ${formatUnit(unit)}` : '-'}</span>
+            <span className={s.answer}>{value ? `${value} ${formatUnit(unit?.display)}` : '-'}</span>
         </p>
     );
 }
