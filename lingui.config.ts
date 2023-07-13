b13115e37ce809972dca027254df2c8e40ab271a
Elena Grednikova <vesnushkalena@gmail.com>
2023-07-13 15:46:02 +0200

    Fix lingui config

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,13 +1,15 @@
-import type { LinguiConfig } from "@lingui/conf";
+import type { LinguiConfig } from '@lingui/conf';
 
 const config: LinguiConfig = {
-  locales: ["en", "ru"],
-  catalogs: [
-    {
-      path: "shared/src/locale/{locale}",
-      include: ["src", "shared"],
-    },
-  ],
+    locales: ['en', 'ru'],
+    format: 'po',
+    catalogs: [
+        {
+            path: '<rootDir>/shared/src/locale/{locale}/messages',
+            include: ['<rootDir>'],
+            exclude: ['**/node_modules/**'],
+        },
+    ],
 };
 
 export default config;
