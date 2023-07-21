d03bcfbab94858e3c5cc416aed55404bb482a082
Elena Grednikova <vesnushkalena@gmail.com>
2023-07-21 22:25:51 +0200

    Make form decorator apply styling

--- src/storybook/decorators/WithQuestionFormProviderDecorator.tsx
+++ src/storybook/decorators/WithQuestionFormProviderDecorator.tsx
@@ -2,12 +2,16 @@ import { Decorator } from '@storybook/react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { FormItems } from 'sdc-qrf';
 
+import s from 'src/components/BaseQuestionnaireResponseForm/BaseQuestionnaireResponseForm.module.scss';
+
 export const WithQuestionFormProviderDecorator: Decorator = (Story) => {
     const methods = useForm<FormItems>();
 
     return (
         <FormProvider {...methods}>
-            <Story />
+            <form className={s.form}>
+                <Story />
+            </form>
         </FormProvider>
     );
 };
