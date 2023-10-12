97fabe31e6f0a870a1e76b2bc0020cfc5e0e6680
Pavel R <prozskov@gmail.com>
2023-10-12 13:39:46 +0200

    Remove unused types

--- src/containers/InvoiceList/types.ts
+++ src/containers/InvoiceList/types.ts
@@ -1,58 +1,11 @@
 import { Invoice } from 'fhir/r4b';
 
-import { ColumnFilterValue, DateColumnFilterValue } from 'src/components/SearchBar/types';
-
 import { SelectOption } from '../OrganizationScheduling/HealthcareServicePractitionerSelect/types';
 
 export interface ModalCancelInvoiceProps {
     onSuccess: () => void;
     invoice: Invoice;
 }
-
-export type InvoiceListFilters = ColumnFilterValue[] &
-    [
-        {
-            id: 'patient';
-            type: 'string';
-            placeholder: string;
-        },
-        {
-            id: 'practitioner';
-            type: 'string';
-            placeholder: string;
-        },
-        {
-            id: 'date';
-            type: 'date';
-            placeholder: [string, string];
-        },
-        {
-            id: 'status';
-            type: 'string';
-            placeholder: string;
-        },
-    ];
-
-export type InvoiceListFilterValues = ColumnFilterValue[] &
-    [
-        {
-            column: InvoiceListFilters[0];
-            value?: string;
-        },
-        {
-            column: InvoiceListFilters[1];
-            value?: string;
-        },
-        {
-            column: InvoiceListFilters[2];
-            value?: DateColumnFilterValue;
-        },
-        {
-            column: InvoiceListFilters[3];
-            value?: string;
-        },
-    ];
-
 export interface InvoiceListSearchBarSelectProps {
     selectedPatient: SelectOption;
     selectedPractitionerRole: SelectOption;
