b6e7a823340ef5553d6233f33c57c83a8a755f67
Ilya Beda <ir4y.ix@gmail.com>
2023-10-23 10:20:30 +1100

    Fix test script run

--- run_testscript.sh
+++ run_testscript.sh
@@ -4,8 +4,6 @@ if [ -f ".env" ]; then
     export `cat .env`
 fi
 
-docker compose -f docker-compose.testscript.yaml up -d
-
-docker-compose -f docker-compose.testscript.yaml exec -it testscript pytest
 docker compose -f docker-compose.testscript.yaml pull --quite
+docker compose -f docker-compose.testscript.yaml up --exit-code-from testscript testscript
 exit $?
