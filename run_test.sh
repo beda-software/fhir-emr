38c47b54b01de662d7f65ce3e1c21ee86d8ee4f0
Ilya Beda <ir4y.ix@gmail.com>
2023-10-23 10:20:19 +1100

    Quite pull

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,7 @@ if [ -f ".env" ]; then
     export `cat .env`
 fi
 
-docker compose -f docker-compose.tests.yaml pull
+docker compose -f docker-compose.tests.yaml pull --quite
 docker compose -f docker-compose.tests.yaml up -d
 
 yarn test
--- run_testscript.sh
+++ run_testscript.sh
@@ -4,8 +4,8 @@ if [ -f ".env" ]; then
     export `cat .env`
 fi
 
-docker compose -f docker-compose.testscript.yaml pull
 docker compose -f docker-compose.testscript.yaml up -d
 
 docker-compose -f docker-compose.testscript.yaml exec -it testscript pytest
+docker compose -f docker-compose.testscript.yaml pull --quite
 exit $?
