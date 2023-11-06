121248a3d52dbdd0dc088f120074c1e87eda294c
Pavel R <prozskov@gmail.com>
2023-11-06 13:02:52 +0100

    Add testTimeout for HealthcareServicePractitionerSelect test

--- src/containers/OrganizationScheduling/HealthcareServicePractitionerSelect/__tests__/HealthcareServicePractitionerSelect.test.ts
+++ src/containers/OrganizationScheduling/HealthcareServicePractitionerSelect/__tests__/HealthcareServicePractitionerSelect.test.ts
@@ -6,109 +6,126 @@ import { getPractitionerRoleNamesMapping, initialSetup, optionExistInOptionList 
 import { useHealthcareServicePractitionerSelect } from '../hooks';
 import { SelectOption } from '../types';
 
+const testTimeout = 30000;
+
 describe('useHealthcareServicePractitionerSelect', () => {
-    it('should fetch healthcareServiceOptions', async () => {
-        const { healthcareServices } = await initialSetup();
-        const { result } = renderHook(() => useHealthcareServicePractitionerSelect());
+    it(
+        'should fetch healthcareServiceOptions',
+        async () => {
+            const { healthcareServices } = await initialSetup();
+            const { result } = renderHook(() => useHealthcareServicePractitionerSelect());
+            const options = await result.current.healthcareServiceOptions('');
 
-        const options = await result.current.healthcareServiceOptions('');
+            const expectOptionToExist = (healthcareService?: HealthcareService) => {
+                const option = { value: healthcareService?.id, label: healthcareService?.name } as SelectOption;
+                expect(optionExistInOptionList(options as SelectOption[], option)).toEqual(true);
+            };
 
-        const expectOptionToExist = (healthcareService?: HealthcareService) => {
-            const option = { value: healthcareService?.id, label: healthcareService?.name } as SelectOption;
-            expect(optionExistInOptionList(options as SelectOption[], option)).toEqual(true);
-        };
+            expect(options.length).toBeGreaterThan(0);
+            expectOptionToExist(healthcareServices?.[0]);
+            expectOptionToExist(healthcareServices?.[1]);
+        },
+        testTimeout,
+    );
 
-        expect(options.length).toBeGreaterThan(0);
-        expectOptionToExist(healthcareServices?.[0]);
-        expectOptionToExist(healthcareServices?.[1]);
-    });
+    it(
+        'should fetch practitionerRoleOptions',
+        async () => {
+            const { practitionerRoles, practitioners } = await initialSetup();
+            const { result } = renderHook(() => useHealthcareServicePractitionerSelect());
+            const practitionerRoleNamesMapping = getPractitionerRoleNamesMapping(practitionerRoles, practitioners);
 
-    it('should fetch practitionerRoleOptions', async () => {
-        const { practitionerRoles, practitioners } = await initialSetup();
-        const { result } = renderHook(() => useHealthcareServicePractitionerSelect());
-        const practitionerRoleNamesMapping = getPractitionerRoleNamesMapping(practitionerRoles, practitioners);
+            const options: SelectOption[] = await result.current.practitionerRoleOptions('');
 
-        const options: SelectOption[] = await result.current.practitionerRoleOptions('');
-
-        expect(options.length).toBeGreaterThan(0);
-        expect(
-            optionExistInOptionList(options, {
-                value: practitionerRoleNamesMapping[0]?.id,
-                label: practitionerRoleNamesMapping[0]?.name,
-            } as SelectOption),
-        ).toEqual(true);
-        expect(
-            optionExistInOptionList(options, {
-                value: practitionerRoleNamesMapping[1]?.id,
-                label: practitionerRoleNamesMapping[1]?.name,
-            } as SelectOption),
-        ).toEqual(true);
-    });
-
-    it('should fetch practitionerRoles based on healthcare service selected', async () => {
-        const { practitionerRoles, practitioners, healthcareServices } = await initialSetup();
-        const { result } = renderHook(() => useHealthcareServicePractitionerSelect());
-        const practitionerRoleNamesMapping = getPractitionerRoleNamesMapping(practitionerRoles, practitioners);
-
-        act(() => {
-            result.current.onChange(
-                { value: healthcareServices?.[0]?.id, label: healthcareServices?.[0]?.name } as SelectOption,
-                'healthcareService',
-            );
-        });
-
-        const options = await result.current.practitionerRoleOptions('');
-
-        expect(options.length).toEqual(1);
-        expect(
-            optionExistInOptionList(
-                options as SelectOption[],
-                {
+            expect(options.length).toBeGreaterThan(0);
+            expect(
+                optionExistInOptionList(options, {
                     value: practitionerRoleNamesMapping[0]?.id,
                     label: practitionerRoleNamesMapping[0]?.name,
-                } as SelectOption,
-            ),
-        ).toEqual(true);
-        expect(
-            optionExistInOptionList(
-                options as SelectOption[],
-                {
+                } as SelectOption),
+            ).toEqual(true);
+            expect(
+                optionExistInOptionList(options, {
                     value: practitionerRoleNamesMapping[1]?.id,
                     label: practitionerRoleNamesMapping[1]?.name,
-                } as SelectOption,
-            ),
-        ).toEqual(false);
-    });
+                } as SelectOption),
+            ).toEqual(true);
+        },
+        testTimeout,
+    );
 
-    it('should fetch healthcareServiceOptions based on selected practitionerRole', async () => {
-        const { healthcareServices, practitionerRoles, practitioners } = await initialSetup();
-        const { result } = renderHook(() => useHealthcareServicePractitionerSelect());
-        const practitionerRoleNamesMapping = getPractitionerRoleNamesMapping(practitionerRoles, practitioners);
+    it(
+        'should fetch practitionerRoles based on healthcare service selected',
+        async () => {
+            const { practitionerRoles, practitioners, healthcareServices } = await initialSetup();
+            const { result } = renderHook(() => useHealthcareServicePractitionerSelect());
+            const practitionerRoleNamesMapping = getPractitionerRoleNamesMapping(practitionerRoles, practitioners);
 
-        act(() => {
-            result.current.onChange(
-                {
-                    value: practitionerRoleNamesMapping?.[0]?.id,
-                    label: practitionerRoleNamesMapping?.[0]?.name,
-                } as SelectOption,
-                'practitionerRole',
-            );
-        });
+            act(() => {
+                result.current.onChange(
+                    { value: healthcareServices?.[0]?.id, label: healthcareServices?.[0]?.name } as SelectOption,
+                    'healthcareService',
+                );
+            });
 
-        const options = await result.current.healthcareServiceOptions('');
+            const options = await result.current.practitionerRoleOptions('');
 
-        expect(options.length).toEqual(1);
-        expect(
-            optionExistInOptionList(
-                options as SelectOption[],
-                { value: healthcareServices?.[0]?.id, label: healthcareServices?.[0]?.name } as SelectOption,
-            ),
-        ).toEqual(true);
-        expect(
-            optionExistInOptionList(
-                options as SelectOption[],
-                { value: healthcareServices?.[1]?.id, label: healthcareServices?.[1]?.name } as SelectOption,
-            ),
-        ).toEqual(false);
-    });
+            expect(options.length).toEqual(1);
+            expect(
+                optionExistInOptionList(
+                    options as SelectOption[],
+                    {
+                        value: practitionerRoleNamesMapping[0]?.id,
+                        label: practitionerRoleNamesMapping[0]?.name,
+                    } as SelectOption,
+                ),
+            ).toEqual(true);
+            expect(
+                optionExistInOptionList(
+                    options as SelectOption[],
+                    {
+                        value: practitionerRoleNamesMapping[1]?.id,
+                        label: practitionerRoleNamesMapping[1]?.name,
+                    } as SelectOption,
+                ),
+            ).toEqual(false);
+        },
+        testTimeout,
+    );
+
+    it(
+        'should fetch healthcareServiceOptions based on selected practitionerRole',
+        async () => {
+            const { healthcareServices, practitionerRoles, practitioners } = await initialSetup();
+            const { result } = renderHook(() => useHealthcareServicePractitionerSelect());
+            const practitionerRoleNamesMapping = getPractitionerRoleNamesMapping(practitionerRoles, practitioners);
+
+            act(() => {
+                result.current.onChange(
+                    {
+                        value: practitionerRoleNamesMapping?.[0]?.id,
+                        label: practitionerRoleNamesMapping?.[0]?.name,
+                    } as SelectOption,
+                    'practitionerRole',
+                );
+            });
+
+            const options = await result.current.healthcareServiceOptions('');
+
+            expect(options.length).toEqual(1);
+            expect(
+                optionExistInOptionList(
+                    options as SelectOption[],
+                    { value: healthcareServices?.[0]?.id, label: healthcareServices?.[0]?.name } as SelectOption,
+                ),
+            ).toEqual(true);
+            expect(
+                optionExistInOptionList(
+                    options as SelectOption[],
+                    { value: healthcareServices?.[1]?.id, label: healthcareServices?.[1]?.name } as SelectOption,
+                ),
+            ).toEqual(false);
+        },
+        testTimeout,
+    );
 });
