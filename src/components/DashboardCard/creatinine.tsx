cc119771ce828819ed8fa872f9310546a9647bc2
Ilya Beda <ir4y.ix@gmail.com>
2024-02-21 15:35:40 +1100

    Add reference values

--- src/components/DashboardCard/creatinine.tsx
+++ src/components/DashboardCard/creatinine.tsx
@@ -1,7 +1,7 @@
 import { InfoOutlined } from '@ant-design/icons';
 import { isSuccess } from 'aidbox-react';
 import { Observation, Patient } from 'fhir/r4b';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ReferenceLine } from 'recharts';
 
 import { RenderRemoteData, WithId } from '@beda.software/fhir-react';
 import { RemoteData } from '@beda.software/remote-data';
@@ -52,12 +52,22 @@ export function CreatinineDashoboard({ observationsRemoteData, patient, reload }
                                     value: valueQuantity?.value,
                                 }));
                                 data.sort((o1, o2) => o1.effective.getTime() - o2.effective.getTime());
+                                const references = {
+                                    female: { min: 0.59, max: 1.04 },
+                                    male: { min: 0.74, max: 1.35 },
+                                    default: { min: 0, max: 0 },
+                                };
+                                const referenceValue = references[patient.gender ?? 'default'] ?? references['default'];
+                                const { min, max } = referenceValue;
+
                                 return data.length > 0 ? (
                                     <LineChart width={600} height={300} data={data}>
                                         <Line type="monotone" dataKey="value" stroke="#8884d8" />
                                         <CartesianGrid stroke="#ccc" />
                                         <XAxis dataKey="effective" />
                                         <YAxis />
+                                        {min > 0 ? <ReferenceLine y={min} label="Min" stroke="red" /> : null}
+                                        {max > 0 ? <ReferenceLine y={max} label="Max" stroke="red" /> : null}
                                         <Tooltip />
                                     </LineChart>
                                 ) : (
