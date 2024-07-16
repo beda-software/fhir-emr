e0bf567859d78887538415965e02a2f21c815353
Elena Grednikova <vesnushkalena@gmail.com>
2024-07-16 13:48:13 +0200

    Make Spinner props not required

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -3,7 +3,7 @@ import { Spin } from 'antd';
 
 export const SpinIndicator = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
-export function Spinner(props: React.HTMLAttributes<HTMLDivElement>) {
+export function Spinner(props?: React.HTMLAttributes<HTMLDivElement>) {
     return (
         <div style={{ padding: 32, display: 'flex', justifyContent: 'center' }} {...props}>
             <Spin indicator={SpinIndicator} />
