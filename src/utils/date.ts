a13dab829f9011963a0c89e29bd28e7f62aae897
Vadim Laletin <rusmiligamer@gmail.com>
2024-07-29 12:31:18 +0200

    Get rid of date-fns (deprecate)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,24 +1,12 @@
-import { differenceInDays, differenceInMonths, differenceInYears, format, parseISO } from 'date-fns';
 import { Period } from 'fhir/r4b';
 import _ from 'lodash';
 
 import { parseFHIRDate, parseFHIRDateTime } from '@beda.software/fhir-react';
 
-export const humanDate = 'dd/MM/yyyy';
+export const humanDate = 'DD MMM YYYY';
 export const humanDateYearMonth = 'MMM YYYY';
 export const humanTime = 'HH:mm';
-export const humanDateTime = 'dd/MM/yyyy HH:mm';
-
-const TIME_FORMAT = 'hh:mm';
-
-const formatFHIRDate = (date: string, formatType: string) => {
-    try {
-        return format(parseISO(date), formatType);
-    } catch {
-        console.error(`Invalid date format: ${date}`);
-        return String(date);
-    }
-};
+export const humanDateTime = 'DD MMM YYYY HH:mm';
 
 export const formatHumanDateTime = (date?: string) => {
     if (!date) {
@@ -26,10 +14,6 @@ export const formatHumanDateTime = (date?: string) => {
     }
 
     return parseFHIRDateTime(date).format(humanDateTime);
-};
-
-export const formatHumanTime = (date: string) => {
-    return formatFHIRDate(date, TIME_FORMAT);
 };
 
 export const formatHumanDate = (date?: string) => {
@@ -53,30 +37,6 @@ export const formatHumanDate = (date?: string) => {
 
     return parseFHIRDateTime(date).format(humanDate);
 };
-
-export function getYears(date: string) {
-    return differenceInYears(new Date(), parseISO(date));
-}
-
-export function getMonths(date: string) {
-    return differenceInMonths(new Date(), parseISO(date));
-}
-
-export function getDays(date: string) {
-    return differenceInDays(new Date(), parseISO(date));
-}
-
-export function getPersonAge(date: string) {
-    if (getYears(date) > 0) {
-        return `${getYears(date)} y.o.`;
-    }
-
-    if (getMonths(date) > 0) {
-        return `${getMonths(date)} m.o.`;
-    }
-
-    return `${getDays(date)} d.o.`;
-}
 
 export function formatPeriodDateTime(period?: Period) {
     const timeRange = _.compact([
