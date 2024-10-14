7bb0b2ea1ea4c640f812837c90641269dbc33751
Демидов Максим <md@imagespark.ru>
2024-10-14 23:25:17 +0300

    add draft Widget for text input inside text #329

--- src/components/BaseQuestionnaireResponseForm/widgets/string.tsx
+++ src/components/BaseQuestionnaireResponseForm/widgets/string.tsx
@@ -1,4 +1,6 @@
+
 import { Form, Input } from 'antd';
+import React, { useEffect, useState } from 'react';
 import { QuestionItemProps } from 'sdc-qrf';
 
 import { useFieldController } from '../hooks';
@@ -20,8 +22,40 @@ export function QuestionText({ parentPath, questionItem }: QuestionItemProps) {
     const fieldName = [...parentPath, linkId, 0, 'value', 'string'];
     const { value, onChange, disabled, formItem, placeholder } = useFieldController(fieldName, questionItem);
 
+    const [parts, setParts] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState<string>('');
+
+    useEffect(() => {
+        const partsArray = value?.split(/<input[^>]*>/g).filter((part: string) => !!part) || [''];
+        setParts(partsArray);
+
+        const match = value?.match(/<input value="([^"]*)"/);
+        setInputValue(match ? match[1] : '');
+    }, [value]);
+
+    const handleInputChange = (newInputValue: string) => {
+        setInputValue(newInputValue);
+
+        const updatedValue = value.replace(/<input[^>]*>/, `<input value="${newInputValue}"/>`);
+        onChange(updatedValue);
+    };
+
     return (
         <Form.Item {...formItem}>
+            {!!parts.length && parts?.map((part, index) => (
+                <React.Fragment key={index}>
+                    {part}
+                    {index < parts.length - 1 && (
+                        <input
+                            style={{ background: 'transparent' }}
+                            type="text"
+                            value={inputValue}
+                            onChange={(e) => handleInputChange(e.target.value)}
+                            placeholder="Введите данные"
+                        />
+                    )}
+                </React.Fragment>
+            ))}
             <Input.TextArea
                 value={value}
                 rows={rowsNumber ?? 1}
