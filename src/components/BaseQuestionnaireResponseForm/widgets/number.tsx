23b63a7ac78509bdd626f1d485aaa8ee670c5edb
atuonufure <atuonufure@gmail.com>
2024-11-05 14:23:14 +0100

    Add unit select to QuestionQuantity widget Refs: https://github.com/beda-software/fhir-emr/issues/323

--- src/components/BaseQuestionnaireResponseForm/widgets/number.tsx
+++ src/components/BaseQuestionnaireResponseForm/widgets/number.tsx
@@ -1,5 +1,6 @@
-import { Form, InputNumber } from 'antd';
+import { Form, InputNumber, Select } from 'antd';
 import { Coding } from 'fhir/r4b';
+import { useState } from 'react';
 import { QuestionItemProps } from 'sdc-qrf';
 
 import { useFieldController } from '../hooks';
@@ -59,10 +60,29 @@ export function QuestionQuantity(props: QuestionItemProps) {
     const fieldName = [...parentPath, linkId, 0, 'value', 'decimal'];
     const { value, onChange, disabled, formItem, placeholder } = useFieldController(fieldName, questionItem);
 
+    const [selectedUnit, setSelectedUnit] = useState(unitOption?.[0]?.display);
+
     return (
         <Form.Item {...formItem}>
             <InputNumber
-                addonAfter={unitOption?.display}
+                addonAfter={
+                    unitOption && unitOption.length > 1 ? (
+                        <Select
+                            value={selectedUnit}
+                            onChange={setSelectedUnit}
+                            style={{ minWidth: 70 }}
+                            disabled={disabled}
+                        >
+                            {unitOption.map((option, index) => (
+                                <Select.Option key={index} value={option.display}>
+                                    {option.display}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                    ) : (
+                        unitOption?.[0]?.display
+                    )
+                }
                 style={inputStyle}
                 disabled={disabled}
                 onChange={onChange}
