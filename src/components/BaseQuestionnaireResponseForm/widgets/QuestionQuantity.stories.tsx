fe04b48b0e47770dab6cc57efaef960d0e7dc68e
atuonufure <atuonufure@gmail.com>
2024-11-05 14:23:42 +0100

    Add stories for QuestionQuantity widget Refs: https://github.com/beda-software/fhir-emr/issues/323

---
+++ src/components/BaseQuestionnaireResponseForm/widgets/QuestionQuantity.stories.tsx
@@ -0,0 +1,69 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { ItemContext } from 'sdc-qrf/lib/types';
+
+import { WithQuestionFormProviderDecorator, withColorSchemeDecorator } from 'src/storybook/decorators';
+
+import { QuestionQuantity } from './index';
+
+const meta: Meta<typeof QuestionQuantity> = {
+    title: 'Questionnaire / questions / quantity',
+    component: QuestionQuantity,
+    decorators: [withColorSchemeDecorator, WithQuestionFormProviderDecorator],
+};
+
+export default meta;
+type Story = StoryObj<typeof QuestionQuantity>;
+
+export const Example: Story = {
+    render: () => (
+        <QuestionQuantity
+            parentPath={[]}
+            questionItem={{
+                text: 'Example',
+                type: 'quantity',
+                linkId: 'example',
+                required: true,
+                unitOption: [
+                    {
+                        code: 'foot',
+                        system: 'http://unitsofmeasure.org',
+                        display: 'ft',
+                    },
+                    {
+                        code: 'inch',
+                        system: 'http://unitsofmeasure.org',
+                        display: 'in',
+                    },
+                    {
+                        code: 'cm',
+                        system: 'http://unitsofmeasure.org',
+                        display: 'cm',
+                    },
+                ],
+            }}
+            context={{} as ItemContext}
+        />
+    ),
+};
+
+export const Single: Story = {
+    render: () => (
+        <QuestionQuantity
+            parentPath={[]}
+            questionItem={{
+                text: 'Example',
+                type: 'quantity',
+                linkId: 'example',
+                required: true,
+                unitOption: [
+                    {
+                        code: 'foot',
+                        system: 'http://unitsofmeasure.org',
+                        display: 'ft',
+                    },
+                ],
+            }}
+            context={{} as ItemContext}
+        />
+    ),
+};
