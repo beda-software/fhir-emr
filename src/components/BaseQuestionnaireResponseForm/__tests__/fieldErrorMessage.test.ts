16f29605b73fa403c629e2b8a0a9ab2522570bb3
Alex Lipovka <alex.lipovka@gmail.com>
2024-11-15 13:47:47 +0400

    Make test more universal for getFieldErrorMessage

--- src/components/BaseQuestionnaireResponseForm/__tests__/fieldErrorMessage.test.ts
+++ src/components/BaseQuestionnaireResponseForm/__tests__/fieldErrorMessage.test.ts
@@ -3,36 +3,58 @@ import { describe } from 'vitest';
 
 import { getFieldErrorMessage } from 'src/components/BaseQuestionnaireResponseForm/utils';
 
+const FIELD_STATE_MESSAGE_MAP = [
+    {
+        fieldName: 'first-name.0.value.string',
+        fieldValue: 'NotSoLongReallyName',
+        fieldText: 'First name',
+        errorMessage: 'first-name.0.value.string must be at most 30 characters',
+        errorType: 'max',
+        expectedResult: 'First name must be at most 30 characters',
+    },
+    {
+        fieldName: 'first-name.0.value.string',
+        fieldValue: 'Basic - 1',
+        fieldText: 'First name',
+        errorMessage: 'first-name.0.value.string is a required field',
+        errorType: 'required',
+        expectedResult: 'First name is a required field',
+    },
+];
+
 describe('Field error message should be relevant and human readable', () => {
-    test('fieldErrorMessage', () => {
-        const field: ControllerRenderProps<FieldValues, any> = {
-            name: 'first-name.0.value.string',
-            value: 'NotSoLongReallyName',
-            onChange: () => {
-                return void 0;
-            },
-            onBlur: () => {
-                return void 0;
-            },
-            ref: () => {
-                return void 0;
-            },
-        };
+    test.each(FIELD_STATE_MESSAGE_MAP)(
+        'getFieldErrorMessage returns correctly transformed message',
+        (fieldStateMessage) => {
+            const field: ControllerRenderProps<FieldValues, any> = {
+                name: fieldStateMessage.fieldName,
+                value: fieldStateMessage.fieldValue,
+                onChange: () => {
+                    return void 0;
+                },
+                onBlur: () => {
+                    return void 0;
+                },
+                ref: () => {
+                    return void 0;
+                },
+            };
 
-        const fieldState: ControllerFieldState = {
-            error: {
-                message: 'first-name.0.value.string must be at most 10 characters',
-                type: 'max',
-            },
-            invalid: true,
-            isTouched: true,
-            isDirty: true,
-        };
+            const fieldState: ControllerFieldState = {
+                error: {
+                    message: fieldStateMessage.errorMessage,
+                    type: fieldStateMessage.errorType,
+                },
+                invalid: true,
+                isTouched: true,
+                isDirty: true,
+            };
 
-        const fieldName = 'First name';
+            const fieldText = fieldStateMessage.fieldText;
 
-        const invalidFieldMessage = getFieldErrorMessage(field, fieldState, fieldName);
+            const invalidFieldMessage = getFieldErrorMessage(field, fieldState, fieldText);
 
-        expect(invalidFieldMessage).toBe('First name must be at most 10 characters');
-    });
+            expect(invalidFieldMessage).toBe(fieldStateMessage.expectedResult);
+        },
+    );
 });
