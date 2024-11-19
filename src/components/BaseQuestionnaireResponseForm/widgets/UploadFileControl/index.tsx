2f74f970b46153edf78322db35acedd1f6ab5174
Ilya Beda <ir4y.ix@gmail.com>
2024-11-19 15:49:31 +1100

    Move component logic to hook

--- src/components/BaseQuestionnaireResponseForm/widgets/UploadFileControl/index.tsx
+++ src/components/BaseQuestionnaireResponseForm/widgets/UploadFileControl/index.tsx
@@ -1,116 +1,22 @@
 import { InboxOutlined } from '@ant-design/icons';
-import { formatError } from 'aidbox-react';
-import { Form, Upload, message, notification } from 'antd';
-import type { UploadFile } from 'antd';
-import { Attachment } from 'fhir/r4b';
-import { useEffect, useRef, useState } from 'react';
+import { Form, Upload } from 'antd';
 import { QuestionItemProps } from 'sdc-qrf';
 
-import { isSuccess } from '@beda.software/remote-data';
-
-import {
-    generateDownloadUrl,
-    generateUploadUrl,
-    uploadFileWithXHR,
-    CustomUploadRequestOption,
-} from 'src/services/file-upload';
-
-import { useFieldController } from '../../hooks';
+import { useUploader } from './hooks';
 
 const { Dragger } = Upload;
 
-type UploadFileProps = QuestionItemProps;
-
-type ValueAttachment = { value: { Attachment: Attachment } };
-
-export function UploadFileControl({ parentPath, questionItem }: UploadFileProps) {
-    const { linkId, helpText, repeats } = questionItem;
-    const fieldName = [...parentPath, linkId];
-    const { formItem, value, onChange } = useFieldController(fieldName, questionItem);
-    /* const download = useRef<Record<string, string>>({}); */
-    const uid = useRef<Record<string, string>>({});
-    const initialFileList: Array<UploadFile> = (value ?? []).map((v: ValueAttachment) => {
-        const url = v.value.Attachment.url!;
-        const file: UploadFile = {
-            uid: url,
-            name: url,
-            percent: 100,
-            /* url: ref.current[url], */
-            /* thumbUrl: ref.current[url], */
-        };
-        return file;
-    });
-    const [fileList, setFileList] = useState<Array<UploadFile>>(initialFileList);
-
-    useEffect(() => {
-        (async () => {
-            const result: Array<UploadFile> = [];
-            for (const f in fileList) {
-                const file = fileList[f]!;
-                if (file.url || file.percent !== 100) {
-                    return file;
-                }
-                const key = uid.current[file.uid] ?? file.uid;
-                const response = await generateDownloadUrl(key);
-                if (isSuccess(response)) {
-                    result.push({
-                        ...file,
-                        url: response.data.downloadUrl,
-                        thumbUrl: response.data.downloadUrl,
-                    });
-                } else {
-                    notification.error(formatError(response.error));
-                    result.push(file);
-                }
-            }
-            setFileList(result);
-        })();
-    }, [JSON.stringify(fileList)]);
-
-    const hasUploadedFile = value?.length > 0;
-
-    const multiple = repeats;
-    const customRequest = async (options: CustomUploadRequestOption) => {
-        const file: UploadFile = options.file as any;
-        const response = await generateUploadUrl(file.name);
-        if (isSuccess(response)) {
-            const { filename, uploadUrl } = response.data;
-            uid.current[file.uid] = filename;
-            uploadFileWithXHR(options, uploadUrl);
-        } else {
-            notification.error(formatError(response.error));
-        }
-    };
-    const onUploaderChange = (info: { fileList: UploadFile<any>[]; file: UploadFile<any> }) => {
-        setFileList(info.fileList);
-        const { status } = info.file;
-        if (status === 'done') {
-            const filename = uid.current[info.file.uid];
-            const attachement = { value: { Attachment: { url: filename } } };
-            if (repeats) {
-                onChange([...value, attachement]);
-            } else {
-                onChange([attachement]);
-            }
-            message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    };
-    const onRemove = (file: UploadFile) => {
-        const filename = uid.current[file.uid] ?? file.uid;
-        onChange((value ?? []).filter(({ value }: ValueAttachment) => value.Attachment.url !== filename));
-        setFileList((files) => files.filter((f) => f.uid !== file.uid));
-    };
-
+export function UploadFileControl(props: QuestionItemProps) {
+    const { showDragger, formItem, customRequest, onChange, onRemove, fileList } = useUploader(props);
+    const { helpText, repeats } = props.questionItem;
     return (
         <Form.Item {...formItem}>
-            {!hasUploadedFile || repeats ? (
+            {showDragger ? (
                 <Dragger
                     listType="picture"
-                    multiple={multiple}
+                    multiple={repeats}
                     customRequest={customRequest}
-                    onChange={onUploaderChange}
+                    onChange={onChange}
                     fileList={fileList}
                     onRemove={onRemove}
                 >
