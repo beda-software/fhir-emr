2ef820217a2bc9c96ecf98ee81b5f75177290a70
Elena Grednikova <vesnushkalena@gmail.com>
2024-11-25 18:04:38 +0100

    Update Sidebar icons

---
+++ src/icons/menu/index.ts
@@ -0,0 +1,12 @@
+export { ScribeIcon } from './ScribeIcon';
+export { UploadIcon } from './UploadIcon';
+export { PatientsIcon } from './PatientsIcon';
+export { EncountersIcon } from './EncountersIcon';
+export { QuestionnairesIcon } from './QuestionnairesIcon';
+export { PractitionersIcon } from './PractitionersIcon';
+export { ServicesIcon } from './ServicesIcon';
+export { InvoicesIcon } from './InvoicesIcon';
+export { MedicationsIcon } from './MedicationsIcon';
+export { PrescriptionsIcon } from './PrescriptionsIcon';
+export { OrganizationsIcon } from './OrganizationsIcon';
+export { DashboardIcon } from './DashboardIcon';
