88c4d04e92bf7b0be8a85b2f7bba1e3e0f979938
Elena Grednikova <vesnushkalena@gmail.com>
2024-12-10 16:47:41 +0100

    Update file uploader stories

--- src/components/BaseQuestionnaireResponseForm/widgets/UploadFileControl/UploadFileControl.stories.tsx
+++ src/components/BaseQuestionnaireResponseForm/widgets/UploadFileControl/UploadFileControl.stories.tsx
@@ -4,6 +4,8 @@ import { ItemContext } from 'sdc-qrf/lib/types';
 import { WithQuestionFormProviderDecorator, withColorSchemeDecorator } from 'src/storybook/decorators';
 
 import { UploadFileControl } from './index';
+import { I18nProvider } from '@lingui/react';
+import { i18n } from '@lingui/core';
 
 const meta: Meta<typeof UploadFileControl> = {
     title: 'Questionnaire / questions / UploadFileControl',
@@ -14,17 +16,21 @@ const meta: Meta<typeof UploadFileControl> = {
 export default meta;
 type Story = StoryObj<typeof UploadFileControl>;
 
+i18n.activate('en');
+
 export const Default: Story = {
     render: () => (
-        <UploadFileControl
-            parentPath={[]}
-            questionItem={{
-                text: 'Upload Document',
-                type: 'attachment',
-                linkId: 'upload-file',
-                required: true,
-            }}
-            context={{} as ItemContext}
-        />
+        <I18nProvider i18n={i18n}>
+            <UploadFileControl
+                parentPath={[]}
+                questionItem={{
+                    text: 'Upload Document',
+                    type: 'attachment',
+                    linkId: 'upload-file',
+                    required: true,
+                }}
+                context={{} as ItemContext}
+            />
+        </I18nProvider>
     ),
 };
