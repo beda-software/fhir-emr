8545c1bc1918941a69ab0f14a1f677c71e66cee0
Elena Tretyakova <vesnushkalena@gmail.com>
2024-12-10 17:10:41 +0100

    Add audio recorder item control component (#403)
    * Add audio recorder item control component
    
    * Update file uploader stories
    
    * Extract locales

---
+++ src/components/AudioRecorder/styles.ts
@@ -0,0 +1,50 @@
+import styled, { css } from 'styled-components';
+
+import { Text } from 'src/components/Typography';
+
+export const S = {
+    Scriber: styled.div`
+        display: flex;
+        flex-direction: column;
+        gap: 8px 0;
+
+        .audio-recorder {
+            width: 100%;
+            box-shadow: none;
+            border-radius: 30px;
+            background-color: ${({ theme }) => theme.neutralPalette.gray_2};
+            padding: 3px 6px 3px 18px;
+        }
+
+        .audio-recorder-timer,
+        .audio-recorder-status {
+            font-family: inherit;
+            color: ${({ theme }) => theme.neutralPalette.gray_12};
+        }
+
+        .audio-recorder-mic {
+            display: none;
+        }
+
+        .audio-recorder-timer {
+            margin-left: 0;
+        }
+
+        .audio-recorder-options {
+            filter: ${({ theme }) => (theme.mode === 'dark' ? `invert(100%)` : `invert(0%)`)};
+        }
+    `,
+    Title: styled(Text)<{ $danger?: boolean }>`
+        font-weight: 700;
+
+        ${({ $danger }) =>
+            $danger &&
+            css`
+                color: ${({ theme }) => theme.antdTheme?.red5};
+            `}
+    `,
+    Audio: styled.audio`
+        height: 52px;
+        width: 100%;
+    `,
+};
