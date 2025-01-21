5bd579b8e6a677862d90ad274f7418a8257e021e
Ilya Beda <ir4y.ix@gmail.com>
2025-01-22 10:40:59 +1100

    WIP table level2

--- src/containers/PatientDetails/new.tsx
+++ src/containers/PatientDetails/new.tsx
@@ -1,12 +1,45 @@
-import { Patient } from 'fhir/r4b';
+import { Encounter, Patient } from 'fhir/r4b';
 
-import { PatientEncounter } from 'src/components';
+import { ResourceListPage } from 'src/components';
 import { DetailPage, Tab } from 'src/uberComponents/DetailPage';
 import { compileAsFirst } from 'src/utils';
 
 import { PatientOverview } from './PatientOverviewDynamic';
 
 const getName = compileAsFirst<Patient, string>("Patient.name.given.first() + ' ' + Patient.name.family");
+
+function PatientEncounter({ patient }: { patient: Patient }) {
+    return (
+        <ResourceListPage<Encounter>
+            headerTitle="Encounters"
+            resourceType="Encounter"
+            searchParams={{ patient: patient.id! }}
+            getTableColumns={() => [
+                {
+                    title: 'Practitioner',
+                    dataIndex: 'practitioner',
+                    key: 'practitioner',
+                    render: (_text: any, { resource }) => JSON.stringify(resource.participant),
+                },
+                {
+                    title: 'Status',
+                    dataIndex: 'status',
+                    key: 'status',
+                    render: (_text: any, { resource }) => {
+                        return resource.status;
+                    },
+                },
+                {
+                    title: 'Date',
+                    dataIndex: 'date',
+                    key: 'date',
+                    width: 250,
+                    render: (_text: any, { resource }) => JSON.stringify(resource.period),
+                },
+            ]}
+        />
+    );
+}
 
 const tabs: Array<Tab<Patient>> = [
     {
@@ -26,7 +59,7 @@ export function NewPatientDetails() {
         <DetailPage<Patient>
             resourceType="Patient"
             getSearchParams={({ id }) => ({ _id: id })}
-            getTitle={({ resource }) => getName(resource)!}
+            getTitle={({ resource, bundle }) => getName(resource, { bundle })!}
             tabs={tabs}
             basePath="patients2"
         />
