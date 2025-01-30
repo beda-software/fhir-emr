7852ee6d67528fca625653a9c4ee284bd9d2b564
Aleksandr Pavlushkin <pavlushkin87@gmail.com>
2025-01-30 22:11:24 +0100

    fix: Fix regression in fhirpath related to functions call
    Improve error handling

--- src/utils/fhirpath.ts
+++ src/utils/fhirpath.ts
@@ -18,10 +18,21 @@ export function evaluate(
         ...FHIRPATH_EVALUATE_INVOCATION_TABLE,
     };
 
-    return fhirpath.evaluate(fhirData, path, context, model, {
-        ...options,
-        userInvocationTable: resultUserInvocationTable,
-    });
+    let customOptions;
+    if (Object.keys(resultUserInvocationTable).length) {
+        customOptions = {
+            ...options,
+            userInvocationTable: resultUserInvocationTable,
+        };
+    } else {
+        customOptions = options;
+    }
+    try {
+        return fhirpath.evaluate(fhirData, path, context, model, customOptions);
+    } catch (e) {
+        console.error('fhirpath.evaluate path "', path, '"', e);
+        return ['Error'];
+    }
 }
 
 export function initFHIRPathEvaluateOptions(userInvocationTable: UserInvocationTable) {
@@ -33,7 +44,8 @@ type NonEmptyArray<T> = [T, ...T[]];
 export function compileAsArray<SRC, DST = unknown, REQ = false>(expression: string) {
     const path = fhirpath.compile(expression);
 
-    return (s: SRC, context?: Context | undefined) => path(s, context) as REQ extends true ? NonEmptyArray<DST> : Array<DST>;
+    return (s: SRC, context?: Context | undefined) =>
+        path(s, context) as REQ extends true ? NonEmptyArray<DST> : Array<DST>;
 }
 
 export function compileAsFirst<SRC, DST = unknown, REQ = false>(expression: string) {
