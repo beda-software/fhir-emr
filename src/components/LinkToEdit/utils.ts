71c0f3bffb9594c53a1f82208e51f8540ae66fb3
Alex Lipovka <alex.lipovka@gmail.com>
2025-02-04 13:23:41 +0400

    Extract entity id by role = source for LinkToEdit

--- src/components/LinkToEdit/utils.ts
+++ src/components/LinkToEdit/utils.ts
@@ -1,15 +1,36 @@
-import { Provenance, Resource } from 'fhir/r4b';
+import { Provenance, Reference, Resource } from 'fhir/r4b';
 import { fromFHIRReference } from 'sdc-qrf';
 
-export function getResourceTypeAndIdFromProvenance(provenance?: Provenance): Resource {
-    if (provenance?.entity?.[0]?.what && 'uri' in provenance.entity[0].what) {
-        const qrURI = provenance.entity[0].what as { uri: string };
-        const qrResourceType = qrURI.uri.split('/')[0] ?? '';
-        const qrId = qrURI.uri.split('/')[1];
+import { compileAsFirst } from 'src/utils';
+
+const getProvenanceEntityUriByRole = (role: string) =>
+    compileAsFirst<Provenance, string>(`Provenance.entity.where(role='${role}').what.uri`);
+
+const getProvenanceEntityReferenceByRole = (role: string) =>
+    compileAsFirst<Provenance, Reference>(`Provenance.entity.where(role='${role}').what.reference`);
+
+const getProvenanceEntityUriSource = getProvenanceEntityUriByRole('source');
+const getProvenanceEntityReferenceSource = getProvenanceEntityReferenceByRole('source');
+
+export function getSourceIdFromProvenance(provenance?: Provenance): Resource | undefined {
+    if (!provenance) {
+        return undefined;
+    }
+
+    const qrURI = getProvenanceEntityUriSource(provenance);
+    if (qrURI) {
+        const qrResourceType = qrURI.split('/')[0] ?? '';
+        const qrId = qrURI.split('/')[1];
         return { resourceType: qrResourceType, id: qrId };
     }
-    return {
-        resourceType: fromFHIRReference(provenance?.entity?.[0]?.what)?.resourceType,
-        id: fromFHIRReference(provenance?.entity?.[0]?.what)?.id,
-    };
+
+    const qrReference = getProvenanceEntityReferenceSource(provenance);
+    if (qrReference) {
+        return {
+            resourceType: fromFHIRReference(qrReference)?.resourceType,
+            id: fromFHIRReference(qrReference)?.id,
+        };
+    }
+
+    return undefined;
 }
