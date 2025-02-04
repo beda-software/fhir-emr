0412417ff321887b9fc975b2063e5edcc34367f3
Alex Lipovka <alex.lipovka@gmail.com>
2025-02-05 00:33:38 +0400

    Export utils from LinkToEdit component

--- src/components/LinkToEdit/index.tsx
+++ src/components/LinkToEdit/index.tsx
@@ -9,19 +9,39 @@ interface LinkToEditProps {
     resource: Resource;
     provenanceList?: Provenance[];
     to?: string;
+    customPathTo?: (props: {
+        provenance: Provenance;
+        customEntityResourceExtract: (provenance: Provenance) => Resource | undefined;
+    }) => string;
+    customEntityResourceExtract?: (provenance?: Provenance) => Resource | undefined;
 }
 
 export function LinkToEdit(props: LinkToEditProps) {
-    const { name, resource, to } = props;
+    const { name, resource, to, customPathTo, customEntityResourceExtract } = props;
     const location = useLocation();
     const provenance = useLinkToEdit({ resourceId: resource.id });
 
-    if (provenance) {
-        const pathname = location.pathname.split('/').slice(0, 3).join('/');
-        const resource = getSourceFromProvenance(provenance);
-        const qrId = resource?.id;
-        return <Link to={to ? `${to}/${qrId}` : `${pathname}/documents/${qrId}`}>{name}</Link>;
+    if (!provenance) {
+        return <>{name}</>;
     }
 
-    return <>{name}</>;
+    if (customPathTo) {
+        return (
+            <Link
+                to={customPathTo({
+                    provenance,
+                    customEntityResourceExtract: customEntityResourceExtract
+                        ? customEntityResourceExtract
+                        : getSourceFromProvenance,
+                })}
+            >
+                {name}
+            </Link>
+        );
+    }
+
+    const pathname = location.pathname.split('/').slice(0, 2).join('/');
+    const entityResource = getSourceFromProvenance(provenance);
+    const qrId = entityResource?.id;
+    return <Link to={to ? `${to}/${qrId}` : `${pathname}/documents/${qrId}`}>{name}</Link>;
 }
