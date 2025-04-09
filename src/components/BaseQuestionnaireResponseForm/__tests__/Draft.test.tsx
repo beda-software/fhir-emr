b5dacbf183381d5d009f053c2e03eef1e6e27657
qscgyjqscgyj <aliestarten@gmail.com>
2025-04-09 11:08:29 +0200

    Added test to check correct draft saving

---
+++ src/components/BaseQuestionnaireResponseForm/__tests__/Draft.test.tsx
@@ -0,0 +1,92 @@
+import { i18n } from '@lingui/core';
+import { I18nProvider } from '@lingui/react';
+import { screen, render, act, fireEvent, waitFor } from '@testing-library/react';
+import { RemoteDataResult } from 'aidbox-react';
+import { Bundle, Patient, Practitioner, QuestionnaireResponse } from 'fhir/r4b';
+import { describe, expect, test, vi } from 'vitest';
+
+import { getFHIRResources } from 'aidbox-react/lib/services/fhir';
+import { axiosInstance } from 'aidbox-react/lib/services/instance';
+
+import { ensure, extractBundleResources, WithId, withRootAccess } from '@beda.software/fhir-react';
+
+import { PatientDocument } from 'src/containers/PatientDetails/PatientDocument';
+import { createPatient, createPractitionerRole, loginAdminUser, waitForAPIProcess } from 'src/setupTests';
+import { ThemeProvider } from 'src/theme';
+
+describe('Draft questionnaire response saves correctly', async () => {
+    async function setup() {
+        await loginAdminUser();
+        return await withRootAccess(axiosInstance, async () => {
+            const patient = await createPatient({
+                name: [{ given: ['John'], family: 'Smith' }],
+            });
+
+            const { practitioner, practitionerRole } = await createPractitionerRole({});
+
+            return { patient, practitioner, practitionerRole };
+        });
+    }
+
+    async function renderForm(patient: Patient, practitioner: WithId<Practitioner>) {
+        const onSuccess = vi.fn();
+
+        act(() => {
+            i18n.activate('en');
+        });
+
+        render(
+            <ThemeProvider>
+                <I18nProvider i18n={i18n}>
+                    <PatientDocument
+                        patient={patient}
+                        author={practitioner}
+                        questionnaireId="repeatable-group"
+                        onSuccess={onSuccess}
+                    />
+                </I18nProvider>
+            </ThemeProvider>,
+        );
+
+        return onSuccess;
+    }
+
+    test('Test there is only one QuestionnaireResponse is saved after instant form submit', async () => {
+        const testFieldValue = 'Test 1';
+
+        const { patient, practitioner } = await setup();
+
+        const onSuccess = await renderForm(patient, practitioner);
+
+        const textField = await screen.findByTestId('repeatable-group-text');
+        expect(textField).toBeEnabled();
+
+        const textInput = textField.querySelector('input')!;
+        act(() => {
+            fireEvent.change(textInput, {
+                target: { value: testFieldValue },
+            });
+        });
+
+        const submitButton = await screen.findByTestId('submit-button');
+        expect(submitButton).toBeEnabled();
+
+        act(() => {
+            fireEvent.click(submitButton);
+        });
+
+        await waitFor(() => expect(onSuccess).toHaveBeenCalled());
+
+        await waitForAPIProcess<RemoteDataResult<Bundle<WithId<QuestionnaireResponse>>>>({
+            service: () =>
+                getFHIRResources('QuestionnaireResponse', {
+                    questionnaire: 'repeatable-group',
+                    _sort: ['-createdAt', '_id'],
+                }),
+            resolver: (result) => {
+                const qrs = extractBundleResources(ensure(result)).QuestionnaireResponse;
+                return qrs.length === 1;
+            },
+        });
+    }, 60000);
+});
