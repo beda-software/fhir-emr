72e0c0a4562bd0400877deedb2535fc279867a7c
Ilya Beda <ir4y.ix@gmail.com>
2025-04-11 10:16:12 +1000

    Describe all three corner cases

--- src/components/BaseQuestionnaireResponseForm/__tests__/Draft.test.tsx
+++ src/components/BaseQuestionnaireResponseForm/__tests__/Draft.test.tsx
@@ -43,6 +43,7 @@ describe('Draft questionnaire response saves correctly', async () => {
                         author={practitioner}
                         questionnaireId="repeatable-group"
                         onSuccess={onSuccess}
+                        autosave={true}
                     />
                 </I18nProvider>
             </ThemeProvider>,
@@ -51,7 +52,38 @@ describe('Draft questionnaire response saves correctly', async () => {
         return onSuccess;
     }
 
-    test('Test there is only one QuestionnaireResponse is saved after instant form submit', async () => {
+    test('Test QuestionnaireResponse autosave', async () => {
+        const testFieldValue = 'Test 1';
+
+        const { patient, practitioner } = await setup();
+
+        await renderForm(patient, practitioner);
+
+        const textField = await screen.findByTestId('repeatable-group-text');
+        expect(textField).toBeEnabled();
+
+        const textInput = textField.querySelector('input')!;
+        act(() => {
+            fireEvent.change(textInput, {
+                target: { value: testFieldValue },
+            });
+        });
+
+        await waitForAPIProcess<RemoteDataResult<Bundle<WithId<QuestionnaireResponse>>>>({
+            service: () =>
+                getFHIRResources('QuestionnaireResponse', {
+                    questionnaire: 'repeatable-group',
+                    status: 'in-progress',
+                    _sort: ['-createdAt', '_id'],
+                }),
+            resolver: (result) => {
+                const qrs = extractBundleResources(ensure(result)).QuestionnaireResponse;
+                return qrs.length === 1;
+            },
+        });
+    }, 60000);
+
+    test('Test QuestionnaireResponse is not duplictated by autosave', async () => {
         const testFieldValue = 'Test 1';
 
         const { patient, practitioner } = await setup();
@@ -77,6 +109,8 @@ describe('Draft questionnaire response saves correctly', async () => {
 
         await waitFor(() => expect(onSuccess).toHaveBeenCalled());
 
+        await new Promise((r) => setTimeout(r, 2000));
+
         await waitForAPIProcess<RemoteDataResult<Bundle<WithId<QuestionnaireResponse>>>>({
             service: () =>
                 getFHIRResources('QuestionnaireResponse', {
@@ -85,6 +119,68 @@ describe('Draft questionnaire response saves correctly', async () => {
                 }),
             resolver: (result) => {
                 const qrs = extractBundleResources(ensure(result)).QuestionnaireResponse;
+                return qrs.length === 1;
+            },
+        });
+    }, 60000);
+
+    test("Test QuestionnaireResponse autosave doesn't reset completed status", async () => {
+        const testFieldValue = 'Test 1';
+
+        const { patient, practitioner } = await setup();
+
+        const onSuccess = await renderForm(patient, practitioner);
+
+        const textField = await screen.findByTestId('repeatable-group-text');
+        expect(textField).toBeEnabled();
+
+        const textInput = textField.querySelector('input')!;
+        act(() => {
+            fireEvent.change(textInput, {
+                target: { value: testFieldValue },
+            });
+        });
+
+        await waitForAPIProcess<RemoteDataResult<Bundle<WithId<QuestionnaireResponse>>>>({
+            service: () =>
+                getFHIRResources('QuestionnaireResponse', {
+                    questionnaire: 'repeatable-group',
+                    status: 'in-progress',
+                    _sort: ['-createdAt', '_id'],
+                }),
+            resolver: (result) => {
+                const qrs = extractBundleResources(ensure(result)).QuestionnaireResponse;
+                return qrs.length === 1;
+            },
+        });
+
+        act(() => {
+            fireEvent.change(textInput, {
+                target: { value: 'update value' },
+            });
+        });
+
+        const submitButton = await screen.findByTestId('submit-button');
+        expect(submitButton).toBeEnabled();
+
+        act(() => {
+            fireEvent.click(submitButton);
+        });
+
+        await waitFor(() => expect(onSuccess).toHaveBeenCalled());
+
+        await new Promise((r) => setTimeout(r, 2000));
+
+        await waitForAPIProcess<RemoteDataResult<Bundle<WithId<QuestionnaireResponse>>>>({
+            service: () =>
+                getFHIRResources('QuestionnaireResponse', {
+                    questionnaire: 'repeatable-group',
+                    status: 'completed',
+                    _sort: ['-createdAt', '_id'],
+                }),
+            resolver: (result) => {
+                const qrs = extractBundleResources(ensure(result)).QuestionnaireResponse;
+                console.log(qrs);
                 return qrs.length === 1;
             },
         });
