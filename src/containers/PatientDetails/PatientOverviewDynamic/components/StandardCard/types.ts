bf5e106df6504546a063dd182e37c00a36a342ff
qscgyjqscgyj <aliestarten@gmail.com>
2025-04-14 19:08:08 +0200

    Added OverviewCardColumn as OverviewCard columns type
    Added OverviewCardColumn as OverviewCard columns type to handle antd
    table columns
    Added urlSearchParams as arg to PrepareFunction

--- src/containers/PatientDetails/PatientOverviewDynamic/components/StandardCard/types.ts
+++ src/containers/PatientDetails/PatientOverviewDynamic/components/StandardCard/types.ts
@@ -1,20 +1,28 @@
+import { ColumnType } from 'antd/lib/table';
 import { Bundle, Resource } from 'fhir/r4b';
 
+interface OverviewCardColumn<T extends Resource | Resource[]> extends ColumnType<T> {
+    key: string;
+    title: string;
+    render: (r: T) => React.ReactNode;
+    width?: string | number;
+}
 export interface OverviewCard<T extends Resource | Resource[]> {
     title: string;
     key: string;
     icon: React.ReactNode;
     data: T[];
     total?: number;
-    columns: {
-        key: string;
-        title: string;
-        render: (r: T) => React.ReactNode;
-        width?: string | number;
-    }[];
+    columns: OverviewCardColumn<T>[];
     getKey: (r: T) => string;
 }
 
 export type PrepareFunction<T extends Resource> =
-    | ((resources: T[], bundle: Bundle<T>, total: number, to?: string) => OverviewCard<T>)
+    | ((
+          resources: T[],
+          bundle: Bundle<T>,
+          total: number,
+          to?: string,
+          urlSearchParams?: URLSearchParams,
+      ) => OverviewCard<T>)
     | ((resources: T[]) => OverviewCard<T[]>);
