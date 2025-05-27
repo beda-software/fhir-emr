d304ccd4a4081126eee729074b5d447492b6bc2e
Vadim Laletin <rusmiligamer@gmail.com>
2025-05-27 17:37:11 +0200

    Pass proper context with fallback

--- src/components/BaseQuestionnaireResponseForm/readonly-widgets/Group/RepeatableGroups.tsx
+++ src/components/BaseQuestionnaireResponseForm/readonly-widgets/Group/RepeatableGroups.tsx
@@ -17,12 +17,22 @@ export function RepeatableGroups(groupItem: GroupItemProps) {
                     return null;
                 }
 
+                if (!context[index]) {
+                    // TODO: it should be impossible, but to be backward compatible - let's see how it works in real life
+                    console.error(
+                        '[SDC-QRF] Failed to get context for item index=',
+                        index,
+                        'linkid=',
+                        linkId,
+                        JSON.stringify(context),
+                    );
+                }
                 return (
                     <QuestionItems
                         key={`${fieldName.join()}-${index}`}
                         questionItems={item!}
                         parentPath={[...parentPath, linkId, 'items', index.toString()]}
-                        context={context[0]!}
+                        context={(context[index] ?? context[0])!}
                     />
                 );
             })}
