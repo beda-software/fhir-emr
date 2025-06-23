5a2db27ecaf5b78046d4f427c81c88f92f70e61d
qscgyjqscgyj <aliestarten@gmail.com>
2025-06-23 13:02:42 +0200

    Removed ai form builder init questionnaire context flow

--- src/containers/QuestionnaireBuilder/context.ts
+++ src/containers/QuestionnaireBuilder/context.ts
@@ -1,4 +1,3 @@
-import { Questionnaire } from 'fhir/r4b';
 import React from 'react';
 
 interface Props {
@@ -9,12 +8,4 @@ interface Props {
 export const FieldSourceContext = React.createContext<Props>({
     moving: 'down',
     setMoving: undefined,
-});
-
-export const AIFormBuilderInitialQuestionnaireContext = React.createContext<Questionnaire>({
-    resourceType: 'Questionnaire',
-    status: 'draft',
-    meta: {
-        profile: ['https://emr-core.beda.software/StructureDefinition/fhir-emr-questionnaire'],
-    },
 });
