5ba2fc8ebcc357bdd854c4c3b42e974989429698
Prosvirin Vladimir <vlprosvirin@gmail.com>
2025-07-03 19:12:45 +0300

    Update markdown context
    ref #https://github.com/breeoot/ehr/issues/1147

--- src/components/BaseQuestionnaireResponseForm/widgets/MDEditorControl/index.tsx
+++ src/components/BaseQuestionnaireResponseForm/widgets/MDEditorControl/index.tsx
@@ -1,21 +1,25 @@
+import { useContext } from 'react';
 import { Form } from 'antd';
 import { QuestionItemProps } from 'sdc-qrf';
 
 import { useFieldController } from 'src/components/BaseQuestionnaireResponseForm/hooks';
 
 import { MarkDownEditor } from './MarkDownEditor';
-import { safeMarkdown } from './utils';
+import { MarkdownProcessorContext } from 'src/contexts/markdown-editor-context';
 
 export function MDEditorControl({ parentPath, questionItem, context }: QuestionItemProps) {
     const { linkId } = questionItem;
     const fieldName = [...parentPath, linkId, 0, 'value', 'string'];
     const { value, onChange, formItem } = useFieldController<string>(fieldName, questionItem);
-    const safeMarkDownString = safeMarkdown(value ?? '');
+
+    const processMarkdown = useContext(MarkdownProcessorContext);
+
+    const processedMarkdown = processMarkdown(value ?? '');
 
     return (
         <Form.Item {...formItem}>
             <MarkDownEditor
-                markdownString={safeMarkDownString}
+                markdownString={processedMarkdown}
                 onChange={onChange}
                 readOnly={questionItem.readOnly}
                 context={context}
