c6bfc7bad2d72a222f36d4b58fedce968ced01dd
Ilya Beda <ir4y.ix@gmail.com>
2025-08-07 22:11:26 +1000

    Refactor invoice list to use uber list component

--- src/containers/InvoiceList/index.tsx
+++ src/containers/InvoiceList/index.tsx
@@ -1,84 +1,158 @@
-import { Trans, t } from '@lingui/macro';
-import { Empty } from 'antd';
+import { t, Trans } from '@lingui/macro';
+import { ColumnsType } from 'antd/lib/table';
+import { Invoice } from 'fhir/r4b';
 
-import { RenderRemoteData } from '@beda.software/fhir-react';
-import { isLoading } from '@beda.software/remote-data';
+import { extractBundleResources } from '@beda.software/fhir-react';
 
-import { Table } from 'src/components';
-import { PageContainer } from 'src/components/BaseLayout/PageContainer';
-import { SpinIndicator, Spinner } from 'src/components/Spinner';
-import { Role, matchCurrentUserRole, selectCurrentUserRoleResource } from 'src/utils/role';
+import { SearchBarColumn, SearchBarColumnType, SorterColumn } from 'src/components/SearchBar/types';
+import { ResourceListPage, questionnaireAction, navigationAction } from 'src/uberComponents/ResourceListPage';
+import { RecordType, TableManager } from 'src/uberComponents/ResourceListPage/types';
+import { formatHumanDateTime } from 'src/utils';
+import { matchCurrentUserRole, Role, selectCurrentUserRoleResource } from 'src/utils/role';
 
-import { InvoiceListSearchBar } from './components/InvoiceListSearchBar';
-import { useInvoiceSearchBarSelect } from './components/InvoiceListSearchBar/hooks';
-import { useInvoicesList } from './hooks';
-import { getInvoiceTableColumns } from './tableUtils';
-import { getSelectedValue } from '../OrganizationScheduling/utils';
+import { InvoiceStatus, InvoiceAmount } from './tableUtils';
+import { getPractitionerName, getInvoicePractitioner, getPatientName, getInvoicePatient } from './utils';
 
 export function InvoiceList() {
-    const {
-        selectedPatient,
-        selectedPractitionerRole,
-        selectedStatus,
-        patientOptions,
-        practitionerRoleOptions,
-        statusOptions,
-        onChange,
-        resetFilter,
-    } = useInvoiceSearchBarSelect();
+    const getFilters = (): SearchBarColumn[] => [
+        {
+            id: 'status',
+            type: SearchBarColumnType.STRING,
+            placeholder: t`Search by status`,
+        },
+        {
+            id: 'subject',
+            type: SearchBarColumnType.REFERENCE,
+            placeholder: t`Search by patient`,
+            expression: 'Patient',
+            path: "name.given.first() + ' ' + name.family",
+        },
+        {
+            id: 'participant',
+            type: SearchBarColumnType.REFERENCE,
+            placeholder: t`Search by practitioner`,
+            expression: 'PractitionerRole',
+            path: 'practitioner.display',
+        },
+    ];
 
-    const selectedPatientValue = matchCurrentUserRole({
-        [Role.Admin]: () => getSelectedValue(selectedPatient),
-        [Role.Patient]: () => selectCurrentUserRoleResource().id,
-        [Role.Practitioner]: () => getSelectedValue(selectedPatient),
-        [Role.Receptionist]: () => getSelectedValue(selectedPatient),
-    });
+    const getSorters = (): SorterColumn[] => [
+        {
+            id: 'date',
+            searchParam: 'date',
+            label: t`Date`,
+        },
+        {
+            id: 'status',
+            searchParam: 'status',
+            label: t`Status`,
+        },
+    ];
 
-    const { invoiceResponse, pagination, handleTableChange, pagerManager } = useInvoicesList(
-        getSelectedValue(selectedPractitionerRole),
-        selectedPatientValue,
-        getSelectedValue(selectedStatus),
-    );
+    const getTableColumns = (manager: TableManager): ColumnsType<RecordType<Invoice>> => [
+        {
+            title: <Trans>Practitioner</Trans>,
+            dataIndex: 'practitioner',
+            key: 'practitioner',
+            width: '15%',
+            render: (_text, record) => {
+                const bundle = record.bundle;
+                const practitioners = extractBundleResources(bundle).Practitioner;
+                const practitionerRoles = extractBundleResources(bundle).PractitionerRole;
+                return getPractitionerName(getInvoicePractitioner(record.resource, practitioners, practitionerRoles));
+            },
+        },
+        {
+            title: <Trans>Patient</Trans>,
+            dataIndex: 'patient',
+            key: 'patient',
+            width: '15%',
+            render: (_text, record) => {
+                const bundle = record.bundle;
+                const patients = extractBundleResources(bundle).Patient;
+                return getPatientName(getInvoicePatient(record.resource, patients));
+            },
+        },
+        {
+            title: <Trans>Date</Trans>,
+            dataIndex: 'date',
+            key: 'date',
+            width: '15%',
+            render: (_text, record) => formatHumanDateTime(record.resource.date),
+        },
+        {
+            title: <Trans>Status</Trans>,
+            dataIndex: 'status',
+            key: 'status',
+            width: '10%',
+            render: (_text, record) => <InvoiceStatus invoice={record.resource} />,
+        },
+        {
+            title: <Trans>Amount</Trans>,
+            dataIndex: 'amount',
+            key: 'amount',
+            width: '10%',
+            render: (_text, record) => <InvoiceAmount invoice={record.resource} />,
+        },
+    ];
+
+    const getRecordActions = (record: RecordType<Invoice>) => {
+        const routeToOpen = record.resource.id!;
+
+        const actions = [navigationAction(<Trans>Open</Trans>, routeToOpen)];
+
+        // Add role-specific actions
+        const roleSpecificActions = matchCurrentUserRole({
+            [Role.Admin]: () => [
+                questionnaireAction(<Trans>Cancel</Trans>, 'cancel-invoice'),
+                questionnaireAction(<Trans>Pay</Trans>, 'pay-invoice'),
+            ],
+            [Role.Patient]: () => [], // Patients only see open action
+            [Role.Practitioner]: () => [
+                questionnaireAction(<Trans>Cancel</Trans>, 'cancel-invoice'),
+                questionnaireAction(<Trans>Pay</Trans>, 'pay-invoice'),
+            ],
+            [Role.Receptionist]: () => [
+                questionnaireAction(<Trans>Cancel</Trans>, 'cancel-invoice'),
+                questionnaireAction(<Trans>Pay</Trans>, 'pay-invoice'),
+            ],
+        });
+
+        return [...actions, ...roleSpecificActions];
+    };
+
+    const getHeaderActions = () => {
+        const author = selectCurrentUserRoleResource();
+
+        return [
+            questionnaireAction(<Trans>Create invoice</Trans>, 'invoice-create', {
+                extra: {
+                    qrfProps: {
+                        launchContextParameters: [{ name: 'Author', resource: author }],
+                    },
+                },
+            }),
+        ];
+    };
 
     return (
-        <PageContainer
-            title={t`Invoices`}
-            layoutVariant="with-table"
-            headerContent={
-                <InvoiceListSearchBar
-                    selectedPatient={selectedPatient}
-                    selectedPractitionerRole={selectedPractitionerRole}
-                    selectedStatus={selectedStatus}
-                    loadPatientOptions={patientOptions}
-                    loadPractitionerRoleOptions={practitionerRoleOptions}
-                    loadStatusOptions={statusOptions}
-                    onChangePatient={(selectedOption) => onChange(selectedOption, 'patient')}
-                    onChangePractitionerRole={(selectedOption) => onChange(selectedOption, 'practitionerRole')}
-                    onChangeStatus={(selectedOption) => onChange(selectedOption, 'status')}
-                    reset={resetFilter}
-                />
-            }
-        >
-            <RenderRemoteData remoteData={invoiceResponse} renderLoading={Spinner}>
-                {({ invoices, practitioners, practitionerRoles, patients }) => (
-                    <Table
-                        pagination={pagination}
-                        onChange={handleTableChange}
-                        bordered
-                        locale={{
-                            emptyText: (
-                                <>
-                                    <Empty description={<Trans>No data</Trans>} image={Empty.PRESENTED_IMAGE_SIMPLE} />
-                                </>
-                            ),
-                        }}
-                        dataSource={invoices}
-                        columns={getInvoiceTableColumns(practitioners, practitionerRoles, patients, pagerManager)}
-                        loading={isLoading(invoiceResponse) && { indicator: SpinIndicator }}
-                        scroll={{ x: 'max-content' }}
-                    />
-                )}
-            </RenderRemoteData>
-        </PageContainer>
+        <ResourceListPage
+            headerTitle={t`Invoices`}
+            resourceType="Invoice"
+            searchParams={{
+                _sort: '-_lastUpdated,_id',
+                _count: 10,
+                _include: [
+                    'Invoice:patient:Patient',
+                    'Invoice:participant:PractitionerRole',
+                    'PractitionerRole:practitioner:Practitioner',
+                ],
+            }}
+            getFilters={getFilters}
+            getSorters={getSorters}
+            getTableColumns={getTableColumns}
+            getRecordActions={getRecordActions}
+            getHeaderActions={getHeaderActions}
+        />
     );
 }
