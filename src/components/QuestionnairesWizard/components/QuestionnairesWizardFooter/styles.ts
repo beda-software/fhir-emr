83afe90efa7edba3ce59eee70b192af67ce59c4c
Alex Lipovka <alex.lipovka@gmail.com>
2025-08-07 21:04:15 +0400

    Tune styles to fit modal form

--- src/components/QuestionnairesWizard/components/QuestionnairesWizardFooter/styles.ts
+++ src/components/QuestionnairesWizard/components/QuestionnairesWizardFooter/styles.ts
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 
-import { footerStyles } from 'src/components/BaseQuestionnaireResponseForm/BaseQuestionnaireResponseForm.styles';
 import { WizardFooter } from 'src/components/Wizard';
 
 export const S = {
     Footer: styled(WizardFooter)`
-        ${footerStyles}
         align-items: flex-end;
         justify-content: space-between;
 
         & > :first-child {
-            padding-bottom: 10px;
+            padding-bottom: 10px 0;
+            margin: 24px 0 0;
+        }
+
+        .ant-modal & > :first-child {
+            padding: 10px 16px;
+            border-top: 1px solid ${({ theme }) => theme.antdTheme?.colorBorderSecondary};
+            margin: 30px -24px -30px;
         }
     `,
 };
