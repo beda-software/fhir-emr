306ca18dfb7d00412676d25a5aa52050c141c9bc
Ilya Beda <ir4y.ix@gmail.com>
2025-08-11 11:11:25 +1000

    Refactor patietn list to use ResourceListPage uber componsnt

--- src/containers/PatientList/index.tsx
+++ src/containers/PatientList/index.tsx
@@ -1,35 +1,162 @@
+import { PlusOutlined } from '@ant-design/icons';
 import { t, Trans } from '@lingui/macro';
-import { Button, Empty, Row, Col, notification } from 'antd';
-import { Patient } from 'fhir/r4b';
-import { useNavigate } from 'react-router-dom';
+import { ColumnsType } from 'antd/lib/table';
+import { Bundle, Consent, Patient } from 'fhir/r4b';
 
-import { isLoading, isSuccess } from '@beda.software/remote-data';
+import { extractBundleResources, parseFHIRReference, SearchParams } from '@beda.software/fhir-react';
 
-import { PageContainer } from 'src/components/BaseLayout/PageContainer';
-import { ModalNewPatient } from 'src/components/ModalNewPatient';
-import { ModalTrigger } from 'src/components/ModalTrigger';
-import { QuestionnaireResponseForm } from 'src/components/QuestionnaireResponseForm';
-import { SearchBar } from 'src/components/SearchBar';
-import { useSearchBar } from 'src/components/SearchBar/hooks';
-import { SpinIndicator } from 'src/components/Spinner';
-import { Table } from 'src/components/Table';
-import { questionnaireIdLoader } from 'src/hooks/questionnaire-response-form-data';
+import { SearchBarColumn, SearchBarColumnType } from 'src/components/SearchBar/types';
+import { ResourceListPage, navigationAction, questionnaireAction } from 'src/uberComponents/ResourceListPage';
+import { RecordType, TableManager } from 'src/uberComponents/ResourceListPage/types';
 import { formatHumanDate } from 'src/utils/date';
 import { renderHumanName } from 'src/utils/fhir';
 import { matchCurrentUserRole, Role } from 'src/utils/role';
 
-import { usePatientList } from './hooks';
-import { getPatientListSearchBarColumns } from './searchBarUtils';
 import { getPatientSearchParamsForPractitioner } from './utils';
 
+function PatientListConsent(props: { searchParams: SearchParams }) {
+    const getFilters = (): SearchBarColumn[] => [
+        {
+            id: 'name',
+            searchParam: 'patient:Patient.name',
+            type: SearchBarColumnType.STRING,
+            placeholder: t`Find patient`,
+            placement: ['search-bar', 'table'],
+        },
+    ];
+
+    const getPatientFromConsent = (consent: Consent, bundle: Bundle): Patient | undefined => {
+        const patients = (extractBundleResources(bundle).Patient ?? []) as Patient[];
+        const patientRef = consent.patient;
+        if (!patientRef) return undefined;
+        const patientId = parseFHIRReference(patientRef).id;
+        return patients.find((p) => p.id === patientId);
+    };
+
+    const getTableColumns = (_manager: TableManager): ColumnsType<RecordType<Consent>> => [
+        {
+            title: <Trans>Name</Trans>,
+            dataIndex: 'name',
+            key: 'name',
+            render: (_text, record) =>
+                renderHumanName(getPatientFromConsent(record.resource, record.bundle)?.name?.[0]),
+            width: 300,
+        },
+        {
+            title: <Trans>Birth date</Trans>,
+            dataIndex: 'birthDate',
+            key: 'birthDate',
+            render: (_text, record) => {
+                const patient = getPatientFromConsent(record.resource, record.bundle);
+                return patient?.birthDate ? formatHumanDate(patient.birthDate) : null;
+            },
+            width: 150,
+        },
+        {
+            title: <Trans>SSN</Trans>,
+            dataIndex: 'identifier',
+            key: 'identifier',
+            render: (_text, record) => {
+                const patient = getPatientFromConsent(record.resource, record.bundle);
+                return patient?.identifier?.find(({ system }) => system === 'http://hl7.org/fhir/sid/us-ssn')?.value;
+            },
+            width: 250,
+        },
+    ];
+
+    const getRecordActions = (record: RecordType<Consent>, _manager: TableManager) => {
+        const patient = getPatientFromConsent(record.resource, record.bundle);
+        return [
+            navigationAction(<Trans>Open</Trans>, `/patients/${patient?.id}`),
+            questionnaireAction(<Trans>Edit</Trans>, 'patient-edit'),
+        ];
+    };
+
+    const getHeaderActions = () => [
+        questionnaireAction(<Trans>Add patient</Trans>, 'patient-create', { icon: <PlusOutlined /> }),
+    ];
+
+    return (
+        <ResourceListPage<Consent>
+            headerTitle={t`Patients`}
+            resourceType="Consent"
+            searchParams={{
+                ...props.searchParams,
+                _sort: '-_lastUpdated,_id',
+                _count: 10,
+            }}
+            getFilters={getFilters}
+            getTableColumns={getTableColumns}
+            getRecordActions={getRecordActions}
+            getHeaderActions={getHeaderActions}
+        />
+    );
+}
+
+function PatientListDefault(props: { searchParams: SearchParams }) {
+    const getFilters = (): SearchBarColumn[] => [
+        {
+            id: 'name',
+            searchParam: 'name',
+            type: SearchBarColumnType.STRING,
+            placeholder: t`Find patient`,
+            placement: ['search-bar', 'table'],
+        },
+    ];
+
+    const getTableColumns = (_manager: TableManager): ColumnsType<RecordType<Patient>> => [
+        {
+            title: <Trans>Name</Trans>,
+            dataIndex: 'name',
+            key: 'name',
+            render: (_text, record) => renderHumanName(record.resource.name?.[0]),
+            width: 300,
+        },
+        {
+            title: <Trans>Birth date</Trans>,
+            dataIndex: 'birthDate',
+            key: 'birthDate',
+            render: (_text, record) => (record.resource.birthDate ? formatHumanDate(record.resource.birthDate) : null),
+            width: 150,
+        },
+        {
+            title: <Trans>SSN</Trans>,
+            dataIndex: 'identifier',
+            key: 'identifier',
+            render: (_text, record) =>
+                record.resource.identifier?.find(({ system }) => system === 'http://hl7.org/fhir/sid/us-ssn')?.value,
+            width: 250,
+        },
+    ];
+
+    const getRecordActions = (record: RecordType<Patient>, _manager: TableManager) => [
+        navigationAction(<Trans>Open</Trans>, `/patients/${record.resource.id}`),
+        questionnaireAction(<Trans>Edit</Trans>, 'patient-edit'),
+    ];
+
+    const getHeaderActions = () => [
+        questionnaireAction(<Trans>Add patient</Trans>, 'patient-create', { icon: <PlusOutlined /> }),
+    ];
+
+    return (
+        <ResourceListPage<Patient>
+            headerTitle={t`Patients`}
+            resourceType="Patient"
+            searchParams={{
+                ...props.searchParams,
+                _sort: '-_lastUpdated,_id',
+                _count: 10,
+            }}
+            getFilters={getFilters}
+            getTableColumns={getTableColumns}
+            getRecordActions={getRecordActions}
+            getHeaderActions={getHeaderActions}
+        />
+    );
+}
+
 export function PatientList() {
-    const navigate = useNavigate();
-
-    const { columnsFilterValues, onChangeColumnFilter, onResetFilters } = useSearchBar({
-        columns: getPatientListSearchBarColumns(),
-    });
-
-    const queryParameters = matchCurrentUserRole({
+    const roleSearchParams = matchCurrentUserRole({
         [Role.Admin]: () => {
             return {};
         },
@@ -44,112 +171,24 @@ export function PatientList() {
         },
     });
 
-    const { patientsResponse, pagerManager, pagination, handleTableChange } = usePatientList(
-        columnsFilterValues,
-        queryParameters,
-    );
+    const forPractitioner = matchCurrentUserRole({
+        [Role.Admin]: () => {
+            return false;
+        },
+        [Role.Practitioner]: (practitioner) => {
+            return true;
+        },
+        [Role.Receptionist]: () => {
+            return false;
+        },
+        [Role.Patient]: () => {
+            return false;
+        },
+    });
 
-    return (
-        <PageContainer
-            layoutVariant="with-table"
-            title={<Trans>Patients</Trans>}
-            titleRightElement={<ModalNewPatient onCreate={pagerManager.reload} />}
-            headerContent={
-                <SearchBar
-                    columnsFilterValues={columnsFilterValues}
-                    onChangeColumnFilter={onChangeColumnFilter}
-                    onResetFilters={onResetFilters}
-                />
-            }
-        >
-            <Table<Patient>
-                pagination={pagination}
-                onChange={handleTableChange}
-                locale={{
-                    emptyText: (
-                        <>
-                            <Empty description={<Trans>No data</Trans>} image={Empty.PRESENTED_IMAGE_SIMPLE} />
-                        </>
-                    ),
-                }}
-                rowKey={(p) => p.id!}
-                dataSource={isSuccess(patientsResponse) ? patientsResponse.data : []}
-                columns={[
-                    {
-                        title: <Trans>Name</Trans>,
-                        dataIndex: 'name',
-                        key: 'name',
-                        render: (_text, resource) => renderHumanName(resource.name?.[0]),
-                    },
-                    {
-                        title: <Trans>Birth date</Trans>,
-                        dataIndex: 'birthDate',
-                        key: 'birthDate',
-                        render: (_text, resource) => (resource.birthDate ? formatHumanDate(resource.birthDate) : null),
-                        width: '25%',
-                    },
-                    {
-                        title: <Trans>SSN</Trans>,
-                        dataIndex: 'identifier',
-                        key: 'identifier',
-                        render: (_text, resource) =>
-                            resource.identifier?.find(({ system }) => system === 'http://hl7.org/fhir/sid/us-ssn')
-                                ?.value,
-                        width: '25%',
-                    },
-                    {
-                        title: <Trans>Actions</Trans>,
-                        dataIndex: 'actions',
-                        key: 'actions',
-                        render: (_text, resource) => {
-                            return (
-                                <Row wrap={false}>
-                                    <Col>
-                                        <Button
-                                            type="link"
-                                            style={{ padding: 0 }}
-                                            onClick={() =>
-                                                navigate(`/patients/${resource.id}`, {
-                                                    state: { resource },
-                                                })
-                                            }
-                                        >
-                                            <Trans>Open</Trans>
-                                        </Button>
-                                    </Col>
-                                    <Col>
-                                        <ModalTrigger
-                                            title={t`Edit patient`}
-                                            trigger={
-                                                <Button type="link">
-                                                    <Trans>Edit</Trans>
-                                                </Button>
-                                            }
-                                        >
-                                            {({ closeModal }) => (
-                                                <QuestionnaireResponseForm
-                                                    questionnaireLoader={questionnaireIdLoader('patient-edit')}
-                                                    launchContextParameters={[{ name: 'Patient', resource }]}
-                                                    onSuccess={() => {
-                                                        notification.success({
-                                                            message: t`Patient saved`,
-                                                        });
-                                                        pagerManager.reload();
-                                                        closeModal();
-                                                    }}
-                                                    onCancel={closeModal}
-                                                />
-                                            )}
-                                        </ModalTrigger>
-                                    </Col>
-                                </Row>
-                            );
-                        },
-                        width: 200,
-                    },
-                ]}
-                loading={isLoading(patientsResponse) && { indicator: SpinIndicator }}
-            />
-        </PageContainer>
+    return forPractitioner ? (
+        <PatientListConsent searchParams={roleSearchParams} />
+    ) : (
+        <PatientListDefault searchParams={roleSearchParams} />
     );
 }
