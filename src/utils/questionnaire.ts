891478eb765ffe284ff7dd093eac6afc56114e2b
Ilya Beda <ir4y.ix@gmail.com>
2025-09-10 13:50:58 +1000

    Fix curly rule

--- src/utils/questionnaire.ts
+++ src/utils/questionnaire.ts
@@ -170,17 +170,23 @@ export function questionnaireItemsToValidationSchema(
             schema = createSchemaArrayOfValues(yup.object({ date: schema }));
         } else if (item.type === 'dateTime') {
             schema = yup.date();
-            if (item.required) schema = schema.required();
+            if (item.required) {
+                schema = schema.required();
+            }
             schema = applyCustomYupTestsToItem(item, schema, customYupTests);
             schema = createSchemaArrayOfValues(yup.object({ dateTime: schema }));
         } else if (item.type === 'time') {
             schema = yup.string().test('time', 'Must be a valid time (HH:mm:ss)', (value) => {
-                if (!value) return true;
+                if (!value) {
+                    return true;
+                }
 
                 const isoTimeRegex = /([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?/;
                 return isoTimeRegex.test(value);
             });
-            if (item.required) schema = schema.required();
+            if (item.required) {
+                schema = schema.required();
+            }
             schema = applyCustomYupTestsToItem(item, schema, customYupTests);
             schema = createSchemaArrayOfValues(yup.object({ time: schema }));
         } else if (item.type === 'group' && item.item) {
