5e373a034600769d02f78742f411bc34ef3fd08c
Alex Lipovka <alex.lipovka@gmail.com>
2025-11-25 17:35:50 +0400

    Update services/fhir.ts to use single axios instance
    Ref:
    - https://github.com/beda-software/fhir-emr/issues/631

--- src/services/fhir.ts
+++ src/services/fhir.ts
@@ -1,19 +1,65 @@
-import config from '@beda.software/emr-config';
-import { initServices } from '@beda.software/fhir-react';
-
-export const {
+import {
+    service as aidboxService,
     axiosInstance,
-    service,
     setInstanceToken,
     resetInstanceToken,
+    setInstanceBaseURL,
+} from 'aidbox-react';
+import type { AxiosRequestConfig } from 'axios';
+
+import { User } from '@beda.software/aidbox-types';
+import { ensure, initServicesFromService } from '@beda.software/fhir-react';
+import { RemoteData, RemoteDataResult, Token } from '@beda.software/remote-data';
+
+export type LoginService = (user: User) => Promise<RemoteData<Token>>;
+
+const fhirService = async <S = any, F = any>(config: AxiosRequestConfig): Promise<RemoteDataResult<S, F>> => {
+    return aidboxService({ ...config, baseURL: axiosInstance.defaults.baseURL + '/fhir' });
+};
+
+export const {
+    createFHIRResource: aidboxCreateFHIRResource,
+    updateFHIRResource: aidboxUpdateFHIRResource,
+    getFHIRResource: aidboxGetFHIRResource,
+    getFHIRResources: aidboxGetFHIRResources,
+    getAllFHIRResources: aidboxGetAllFHIRResources,
+    findFHIRResource: aidboxFindFHIRResource,
+    saveFHIRResource: aidboxSaveFHIRResource,
+    saveFHIRResources: aidboxSaveFHIRResources,
+    patchFHIRResource: aidboxpatchFHIRResource,
+    deleteFHIRResource: aidboxDeleteFHIRResource,
+    forceDeleteFHIRResource: aidboxForceDeleteFHIRResource,
+    getConcepts: aidboxGetConcepts,
+    applyFHIRService: aidboxApplyFHIRService,
+    applyFHIRServices: aidboxApplyFHIRServices,
+} = initServicesFromService(aidboxService);
+
+export const {
+    createFHIRResource,
+    updateFHIRResource,
     getFHIRResource,
     getFHIRResources,
     getAllFHIRResources,
+    findFHIRResource,
     saveFHIRResource,
     saveFHIRResources,
-    updateFHIRResource,
-    createFHIRResource,
-    forceDeleteFHIRResource,
     patchFHIRResource,
-    setInstanceBaseURL,
-} = initServices(config.fhirBaseURL ? config.fhirBaseURL : config.baseURL + '/fhir');
+    deleteFHIRResource,
+    forceDeleteFHIRResource,
+    getConcepts,
+    applyFHIRService,
+    applyFHIRServices,
+    service,
+} = initServicesFromService(fhirService);
+
+export async function getAidboxToken(user: User, loginService: LoginService): Promise<Token> {
+    if (!user.email) {
+        throw new Error('Can not login for user without an email');
+    }
+
+    const result = await loginService(user);
+
+    return ensure(result);
+}
+
+export { aidboxService, axiosInstance, setInstanceToken, resetInstanceToken, setInstanceBaseURL };
