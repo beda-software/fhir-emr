eda8d22b35eea37767b9471dcae43b21d72aab63
Ilya Beda <ir4y.ix@gmail.com>
2025-12-10 09:37:28 +1100

    Use getFHIRResource to load patient
    Remove care plan loadin

--- src/containers/PatientDetails/index.tsx
+++ src/containers/PatientDetails/index.tsx
@@ -1,3 +1,4 @@
+import { useService } from 'aidbox-react';
 import { CarePlan, Patient } from 'fhir/r4b';
 import { useMemo } from 'react';
 import { useParams, Outlet, Route, Routes, useNavigate } from 'react-router-dom';
@@ -11,11 +12,11 @@ import { PatientEncounter } from 'src/components/PatientEncounter';
 import { Spinner } from 'src/components/Spinner';
 import { PatientReloadProvider } from 'src/containers/PatientDetails/Dashboard/contexts';
 import { PatientDocumentWizard } from 'src/containers/PatientDetails/PatientDocumentWizard';
+import { getFHIRResource } from 'src/services';
 import { sharedAuthorizedPractitionerRoles } from 'src/sharedState';
 import { renderHumanName } from 'src/utils';
 import { matchCurrentUserRole, selectCurrentUserRoleResource, Role } from 'src/utils/role';
 
-import { usePatientResource } from './hooks';
 import { PatientApps } from './PatientApps';
 import { PatientDetailsTabs } from './PatientDetailsTabs';
 import { PatientDocument } from './PatientDocument';
@@ -41,17 +42,19 @@ export const PatientDetails = (props: PatientDetailsProps) => {
     const { isDefaultRoutesDisabled } = props;
     const navigate = useNavigate();
 
-    const [patientResponse, manager] = usePatientResource({ id: params.id! });
+    const [patientResponse, manager] = useService(() =>
+        getFHIRResource<Patient>({ reference: `Patient/${params.id!}` }),
+    );
     const author = selectCurrentUserRoleResource();
     const embeddedPages = useMemo(() => {
         if (isSuccess(patientResponse)) {
-            return props.embeddedPages?.(patientResponse.data.patient, patientResponse.data.carePlans);
+            return props.embeddedPages?.(patientResponse.data, []);
         }
     }, [patientResponse]);
 
     return (
         <RenderRemoteData remoteData={patientResponse} renderLoading={Spinner}>
-            {({ patient }) => {
+            {(patient) => {
                 return (
                     <PatientReloadProvider reload={manager.softReloadAsync}>
                         <PageContainer
