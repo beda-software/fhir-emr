9f2a98e6d65ca23542ee4193f269e2238f63fd70
Alex Lipovka <alex.lipovka@gmail.com>
2026-01-20 22:01:28 +0400

    Revert edits to RepeatableGroupCard

--- src/components/BaseQuestionnaireResponseForm/widgets/Group/RepeatableGroups/RepeatableGroupCard/index.tsx
+++ src/components/BaseQuestionnaireResponseForm/widgets/Group/RepeatableGroups/RepeatableGroupCard/index.tsx
@@ -1,5 +1,4 @@
 import { t } from '@lingui/macro';
-import { useCallback } from 'react';
 import { QuestionItems } from 'sdc-qrf';
 
 import { useRepeatableGroup } from './hooks';
@@ -8,33 +7,27 @@ import { RepeatableGroupProps } from '../types';
 
 interface Props extends RepeatableGroupProps {
     variant?: 'main-card' | 'sub-card';
-    onRemove?: () => void;
 }
 
 export function RepeatableGroupCard(props: Props) {
-    const { index, groupItem, variant, onRemove: onRemoveProp } = props;
+    const { index, groupItem, variant } = props;
     const { questionItem } = groupItem;
     const { item, text, readOnly } = questionItem;
 
     const { onRemove, parentPath, context } = useRepeatableGroup(props);
 
-    const handleRemove = useCallback(() => {
-        onRemove();
-        onRemoveProp?.();
-    }, [onRemove, onRemoveProp]);
-
     const title = `${text || t`Item`} ${index + 1}`;
 
     if (variant === 'sub-card') {
         return (
-            <GroupSubCard title={title} onRemove={handleRemove} readOnly={readOnly}>
+            <GroupSubCard title={title} onRemove={onRemove} readOnly={readOnly}>
                 <QuestionItems questionItems={item!} parentPath={parentPath} context={context} />
             </GroupSubCard>
         );
     }
 
     return (
-        <GroupMainCard title={title} onRemove={handleRemove} readOnly={readOnly}>
+        <GroupMainCard title={title} onRemove={onRemove} readOnly={readOnly}>
             <QuestionItems questionItems={item!} parentPath={parentPath} context={context} />
         </GroupMainCard>
     );
