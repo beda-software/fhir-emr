8f980d6773dd488ce4b0d3b89e130e6cc04f0583
Alex Lipovka <alex.lipovka@gmail.com>
2026-02-03 20:09:28 +0400

    Remove unused props from GroupTabs

--- src/components/BaseQuestionnaireResponseForm/widgets/GroupTabs/index.tsx
+++ src/components/BaseQuestionnaireResponseForm/widgets/GroupTabs/index.tsx
@@ -1,19 +1,14 @@
 import { Tabs } from 'antd';
 import type { Tab } from 'rc-tabs/lib/interface';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useWatch } from 'react-hook-form';
-import { FCEQuestionnaireItem, GroupItemProps, QuestionItems, getEnabledQuestions } from 'sdc-qrf';
-
-type TabItem = Tab & {
-    linkId: string;
-    status: 'process' | 'error';
-};
+import { GroupItemProps, QuestionItems, getEnabledQuestions } from 'sdc-qrf';
 
 export function GroupTabs(props: GroupItemProps) {
     const { parentPath, questionItem, context } = props;
 
     if (questionItem.repeats) {
-        console.warn('GroupWizard does not support repeatable groups in the first level');
+        console.warn('GroupTabs does not support repeatable groups in the first level');
     }
     const groupContext = context[0]!;
 
@@ -25,26 +20,23 @@ export function GroupTabs(props: GroupItemProps) {
 
     const tabItems = useMemo(() => item.filter((i) => !i.hidden), [item]);
 
-    const getTabItems = useCallback(
-        (item: FCEQuestionnaireItem): TabItem => {
-            return {
-                key: item.linkId,
-                label: item.text,
-                status: 'error',
-                linkId: item.linkId,
-                children: (
-                    <QuestionItems
-                        questionItems={item.item!}
-                        parentPath={[...parentPath, linkId, 'items', item.linkId, 'items']}
-                        context={groupContext}
-                    />
-                ),
-            };
-        },
-        [parentPath, linkId, groupContext],
+    const tabsItems: Tab[] = useMemo(
+        () =>
+            tabItems.map((item) => {
+                return {
+                    key: item.linkId,
+                    label: item.text,
+                    children: (
+                        <QuestionItems
+                            questionItems={item.item!}
+                            parentPath={[...parentPath, linkId, 'items', item.linkId, 'items']}
+                            context={groupContext}
+                        />
+                    ),
+                };
+            }),
+        [groupContext, linkId, parentPath, tabItems],
     );
-
-    const tabsItems: TabItem[] = useMemo(() => tabItems.map((item) => getTabItems(item)), [getTabItems, tabItems]);
 
     return <Tabs type="card" items={tabsItems} />;
 }
