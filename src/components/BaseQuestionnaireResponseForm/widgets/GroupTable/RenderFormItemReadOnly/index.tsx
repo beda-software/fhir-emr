a62d564a818e31d652364697b2f49078b992ee8b
Alex Lipovka <alex.lipovka@gmail.com>
2026-02-11 21:18:36 +0400

    Refactor GroupTable value getting

--- src/components/BaseQuestionnaireResponseForm/widgets/GroupTable/RenderFormItemReadOnly/index.tsx
+++ src/components/BaseQuestionnaireResponseForm/widgets/GroupTable/RenderFormItemReadOnly/index.tsx
@@ -1,145 +1,41 @@
 import _ from 'lodash';
-import { AnswerValue, FCEQuestionnaireItem, FormItems, getAnswerValues, isAnswerValueEmpty } from 'sdc-qrf';
+import { FCEQuestionnaireItem, FormItems, getAnswerValues, isAnswerValueEmpty } from 'sdc-qrf';
 
-import { parseFHIRReference } from '@beda.software/fhir-react';
-
-import { formatHumanDate, formatHumanDateTime, formatHumanTime } from 'src/utils';
-
-import { isFormAnswerItems } from '../utils';
-
-interface RenderQuestionnaireItemProps {
-    items: AnswerValue[];
-}
-
-const RenderString = ({ items }: RenderQuestionnaireItemProps) => {
-    return items.map((item) => item?.string || '-').join(', ');
-};
-
-const RenderDecimal = ({ items }: RenderQuestionnaireItemProps) => {
-    return items.map((item) => item?.decimal?.toString() ?? '-').join(', ');
-};
-
-const RenderInteger = ({ items }: RenderQuestionnaireItemProps) => {
-    return items.map((item) => item?.integer?.toString() ?? '-').join(', ');
-};
-
-const RenderDate = ({ items }: RenderQuestionnaireItemProps) => {
-    return items.map((item) => formatHumanDate(item?.date) ?? '-').join(', ');
-};
-
-const RenderDateTime = ({ items }: RenderQuestionnaireItemProps) => {
-    return items.map((item) => formatHumanDateTime(item?.dateTime) ?? '-').join(', ');
-};
-
-const RenderTime = ({ items }: RenderQuestionnaireItemProps) => {
-    return items.map((item) => formatHumanTime(item?.time) ?? '-').join(', ');
-};
-
-const RenderBoolean = ({ items }: RenderQuestionnaireItemProps) => {
-    return items.map((item) => (item?.boolean ? 'Yes' : 'No' ?? '-')).join(', ');
-};
-
-const RenderReference = ({ items }: RenderQuestionnaireItemProps) => {
-    return items
-        .map((item) => {
-            if (!item.Reference) {
-                return '-';
-            }
-            return item.Reference.display ?? parseFHIRReference(item.Reference).id ?? '-';
-        })
-        .join(', ');
-};
-
-const RenderQuantity = ({ items }: RenderQuestionnaireItemProps) => {
-    return items
-        .map((item) => {
-            if (!item.Quantity) {
-                return '-';
-            }
-            const value = item.Quantity.value;
-            const unit = item.Quantity.unit;
-            return value?.toString() + (unit ? ' ' + unit : '') ?? '-';
-        })
-        .join(', ');
-};
-
-const RenderAttachment = ({ items }: RenderQuestionnaireItemProps) => {
-    return items
-        .map((item) => {
-            if (!item.Attachment) {
-                return '-';
-            }
-            const title = item.Attachment.title;
-            return title ?? '-';
-        })
-        .join(', ');
-};
-
-const RenderCoding = ({ items }: RenderQuestionnaireItemProps) => {
-    return items
-        .map((item) => {
-            if (!item.Coding) {
-                return '-';
-            }
-            const display = item.Coding.display;
-            const code = item.Coding.code;
-            return display ?? code ?? '-';
-        })
-        .join(', ');
-};
+import { getValueFromAnswerValue, isFormAnswerItems, substituteUndefined } from '../utils';
 
 export const RenderFormItemReadOnly = (props: {
     formItem: FormItems | undefined | null;
     questionnaireItem: FCEQuestionnaireItem | undefined | null;
 }) => {
     const { formItem, questionnaireItem } = props;
+    const emptySymbol = '-';
 
     if (!formItem || !questionnaireItem) {
-        return '-';
+        return emptySymbol;
     }
 
     const questionnaireItemType = questionnaireItem.type;
 
     if (!isFormAnswerItems(formItem)) {
-        return '-';
+        return emptySymbol;
     }
     const answerValues = getAnswerValues(formItem);
 
     if (answerValues.length === 0) {
-        return '-';
+        return emptySymbol;
     }
 
     if (_.some(answerValues, (answerValue) => isAnswerValueEmpty(answerValue))) {
-        return '-';
+        return emptySymbol;
     }
-    switch (questionnaireItemType) {
-        case 'string':
-            return <RenderString items={answerValues} />;
-        case 'boolean':
-            return <RenderBoolean items={answerValues} />;
-        case 'date':
-            return <RenderDate items={answerValues} />;
-        case 'dateTime':
-            return <RenderDateTime items={answerValues} />;
-        case 'decimal':
-            return <RenderDecimal items={answerValues} />;
-        case 'integer':
-            return <RenderInteger items={answerValues} />;
-        case 'time':
-            return <RenderTime items={answerValues} />;
-        case 'text':
-            return <RenderString items={answerValues} />;
-        case 'choice':
-            return <RenderCoding items={answerValues} />;
-        case 'open-choice':
-            return <RenderCoding items={answerValues} />;
-        case 'attachment':
-            return <RenderAttachment items={answerValues} />;
-        case 'reference':
-            return <RenderReference items={answerValues} />;
-        case 'quantity':
-            return <RenderQuantity items={answerValues} />;
-        default:
-            return '-';
-    }
+
+    return (
+        <>
+            {answerValues
+                .map((value) =>
+                    substituteUndefined(getValueFromAnswerValue(value, questionnaireItemType, true), emptySymbol),
+                )
+                .join(', ')}
+        </>
+    );
 };
--- src/components/BaseQuestionnaireResponseForm/widgets/GroupTable/utils.ts
+++ src/components/BaseQuestionnaireResponseForm/widgets/GroupTable/utils.ts
@@ -1,42 +1,79 @@
 import { QuestionnaireItem } from 'fhir/r4b';
 import { AnswerValue, FormAnswerItems, FormGroupItems, getAnswerValues } from 'sdc-qrf';
 
+import { parseFHIRReference } from '@beda.software/fhir-react';
+
+import { formatHumanDate, formatHumanDateTime, formatHumanTime } from 'src/utils';
+
 export const isFormAnswerItems = (
     item: FormGroupItems | (FormAnswerItems | undefined)[] | undefined,
 ): item is FormAnswerItems[] => {
     return Array.isArray(item) && item.every((item) => item !== undefined && item !== null && 'value' in item);
 };
 
-const getValueFromAnswerValue = (answerValue: AnswerValue, questionnaireItemType: QuestionnaireItem['type']) => {
+export const substituteUndefined = (value: string | number | boolean | undefined, symbol = '-') =>
+    value === undefined ? symbol : value;
+
+export const getValueFromAnswerValue = (
+    answerValue: AnswerValue,
+    questionnaireItemType: QuestionnaireItem['type'],
+    preFormat = false,
+) => {
     switch (questionnaireItemType) {
         case 'string':
+        case 'text':
             return answerValue.string;
         case 'boolean':
-            return answerValue.boolean;
+            if (!preFormat) {
+                return answerValue.boolean;
+            }
+            if (answerValue.boolean === undefined) {
+                return undefined;
+            }
+            return answerValue.boolean ? 'Yes' : 'No';
         case 'date':
-            return answerValue.date;
+            if (!preFormat) {
+                return answerValue.date;
+            }
+            return formatHumanDate(answerValue.date);
         case 'dateTime':
-            return answerValue.dateTime;
+            if (!preFormat) {
+                return answerValue.dateTime;
+            }
+            return formatHumanDateTime(answerValue.dateTime);
+        case 'time':
+            if (!preFormat) {
+                return answerValue.time;
+            }
+            return formatHumanTime(answerValue.time);
         case 'decimal':
             return answerValue.decimal;
         case 'integer':
             return answerValue.integer;
-        case 'time':
-            return answerValue.time;
-        case 'text':
-            return answerValue.string;
         case 'choice':
-            return answerValue.Coding?.code;
         case 'open-choice':
-            return answerValue.Coding?.code;
+            return answerValue.Coding?.display ?? answerValue.Coding?.code;
         case 'attachment':
             return answerValue.Attachment?.title;
         case 'reference':
-            return answerValue.Reference?.display;
+            if (!preFormat) {
+                return answerValue.Reference?.display;
+            }
+            return (
+                answerValue.Reference?.display ??
+                (answerValue.Reference ? parseFHIRReference(answerValue.Reference).id : undefined)
+            );
         case 'quantity':
-            return answerValue.Quantity?.value;
+            if (!preFormat) {
+                return answerValue.Quantity?.value;
+            }
+            return answerValue.Quantity
+                ? `${answerValue.Quantity.value ?? ''}${
+                      answerValue.Quantity.unit ? ' ' + answerValue.Quantity.unit : ''
+                  }`.trim()
+                : undefined;
         default:
-            return '-';
+            return undefined;
     }
 };
 
@@ -49,5 +86,5 @@ export const getFormAnswerItemFirstValue = (
         return null;
     }
     const firstValue = answerValues[0];
-    return getValueFromAnswerValue(firstValue, questionnaireItemType);
+    return getValueFromAnswerValue(firstValue, questionnaireItemType, false);
 };
