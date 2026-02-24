1a386dfbda6987547d1856f0b698745ac2f127d9
Alex Lipovka <alex.lipovka@gmail.com>
2026-02-24 09:02:26 +0400

    Add filtering feature to GroupTable itemControl (#718)
    * Update sdc-qrf to support enableFiltering
    
    * Add support for filters to GroupTable
    
    * Refactor GroupTable filters to separate hook
    
    * Refactor group table filtering logic
    
    Renames `isTableItemFiltered` to `isTableItemMatchesFilter` for clarity.
    Simplifies the implementation of filter change handling and updates
    related type annotations.
    
    * Refactor GroupTable column filtering logic
    
    Update filtering logic for GroupTable columns to correctly apply filters
    and remove unnecessary type casting.
    
    * Switch to splitstring searchbarcolumn by default in GroupTable
    
    * Update locale

--- src/components/BaseQuestionnaireResponseForm/widgets/GroupTable/utils.ts
+++ src/components/BaseQuestionnaireResponseForm/widgets/GroupTable/utils.ts
@@ -1,5 +1,7 @@
+import type { ColumnType, ColumnsType } from 'antd/es/table/interface';
 import { QuestionnaireItem } from 'fhir/r4b';
 import _ from 'lodash';
+import moment from 'moment';
 import {
     AnswerValue,
     FCEQuestionnaireItem,
@@ -14,9 +16,25 @@ import {
 
 import { parseFHIRReference } from '@beda.software/fhir-react';
 
+import {
+    ColumnFilterValue,
+    SearchBarColumn,
+    SearchBarColumnType,
+    isChoiceColumn,
+    isDateColumn,
+    isDateColumnFilterValue,
+    isReferenceColumn,
+    isSingleDateColumn,
+    isSingleDateColumnFilterValue,
+    isSolidChoiceColumn,
+    isSplitStringColumn,
+    isSplitStringColumnFilterValue,
+    isStringColumn,
+    isStringColumnFilterValue,
+} from 'src/components/SearchBar/types';
 import { formatHumanDate, formatHumanDateTime, formatHumanTime } from 'src/utils';
 
-import { GroupTableRow } from './types';
+import { GroupTableItem, GroupTableRow } from './types';
 
 export const isFormAnswerItems = (
     item: FormGroupItems | (FormAnswerItems | undefined)[] | undefined,
@@ -157,4 +175,121 @@ export const getDataSource = (
     }
 
     return [];
+};
+
+export const getSearchBarColumnType = (questionItem: FCEQuestionnaireItem): SearchBarColumn => {
+    const type = questionItem.type;
+    switch (type) {
+        case 'date':
+        case 'dateTime':
+            return {
+                id: questionItem.linkId,
+                type: SearchBarColumnType.DATE,
+                placeholder: ['From...', 'To...'],
+            };
+        case 'choice':
+        case 'open-choice':
+        case 'time':
+        case 'reference':
+        case 'boolean':
+        case 'decimal':
+        case 'integer':
+        case 'attachment':
+        case 'quantity':
+        case 'string':
+        case 'text':
+        default:
+            return {
+                id: questionItem.linkId,
+                placeholder: 'Search...',
+                type: SearchBarColumnType.SPLITSTRING,
+                searchBehavior: 'AND',
+                separator: ' ',
+            };
+    }
+};
+
+export const getTableItemValue = (
+    formItem: FormGroupItems | (FormAnswerItems | undefined)[] | undefined,
+    questionItem: FCEQuestionnaireItem,
+) => {
+    if (!isFormAnswerItems(formItem)) {
+        return undefined;
+    }
+    const answerValue = getAnswerValues(formItem);
+    if (!answerValue[0]) {
+        return undefined;
+    }
+    const value = getValueFromAnswerValue(answerValue[0], questionItem.type);
+    return value;
+};
+
+export const isColumnTypeArray = (column: ColumnsType<GroupTableRow>): column is ColumnType<GroupTableRow>[] => {
+    return 'dataIndex' in column;
+};
+
+export const isTableItemMatchesFilter = (item?: GroupTableItem, filterValue?: ColumnFilterValue) => {
+    if (!item || !item.formItem || !item.questionnaireItem || !filterValue) {
+        return true;
+    }
+    const itemValue = getTableItemValue(item.formItem, item.questionnaireItem);
+
+    if (!itemValue) {
+        return true;
+    }
+
+    if (isStringColumnFilterValue(filterValue) && _.isString(itemValue)) {
+        const value = filterValue.value;
+        return value && !_.isEmpty(value) ? itemValue.toLowerCase().includes(value.toLowerCase()) : true;
+    }
+
+    if (isSplitStringColumnFilterValue(filterValue) && _.isString(itemValue)) {
+        if (filterValue.column.searchBehavior === 'AND') {
+            const values = filterValue.value?.split(filterValue.column.separator ?? ' ') ?? [];
+            return values?.every((value) => itemValue.toLowerCase().includes(value.toLowerCase()));
+        } else if (filterValue.column.searchBehavior === 'OR') {
+            const values = filterValue.value?.split(filterValue.column.separator ?? ' ') ?? [];
+            return values?.some((value) => itemValue.toLowerCase().includes(value.toLowerCase()));
+        }
+    }
+
+    if (isDateColumnFilterValue(filterValue) && _.isString(itemValue)) {
+        const fromDate = filterValue.value?.[0];
+        const toDate = filterValue.value?.[1];
+        const testValue = moment(itemValue);
+        return testValue.isBetween(fromDate, toDate, undefined, '[]');
+    }
+
+    if (isSingleDateColumnFilterValue(filterValue) && _.isString(itemValue)) {
+        const testValue = moment(itemValue);
+        return testValue.isSameOrAfter(filterValue.value) && testValue.isSameOrBefore(filterValue.value);
+    }
+
+    return true;
+};
+
+export const createColumnFilterValue = (column: SearchBarColumn): ColumnFilterValue => {
+    if (isStringColumn(column)) {
+        return { column };
+    }
+    if (isDateColumn(column)) {
+        return { column };
+    }
+    if (isSingleDateColumn(column)) {
+        return { column };
+    }
+    if (isReferenceColumn(column)) {
+        return { column };
+    }
+    if (isChoiceColumn(column)) {
+        return { column };
+    }
+    if (isSolidChoiceColumn(column)) {
+        return { column };
+    }
+    if (isSplitStringColumn(column)) {
+        return { column };
+    }
+
+    throw new Error('Unsupported column type');
 };
