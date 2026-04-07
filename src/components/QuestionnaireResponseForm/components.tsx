6d5c385c0522d52a56291870a3487cc9f83a960a
Alex Lipovka <alex.lipovka@gmail.com>
2026-04-07 22:25:30 +0400

    Add ReadonlyQuestionnaireResponseForm

--- src/components/QuestionnaireResponseForm/components.tsx
+++ src/components/QuestionnaireResponseForm/components.tsx
@@ -2,14 +2,20 @@ import {
     QuestionnaireResponseForm as FHIRQuestionnaireResponseForm,
     Props,
 } from '@beda.software/fhir-questionnaire/components/QuestionnaireResponseForm';
+import { persistSaveQuestionnaireReponseService } from '@beda.software/fhir-questionnaire/components/QuestionnaireResponseForm/questionnaire-response-form-data';
 
 import {
     groupControlComponents,
     itemComponents,
     itemControlComponents,
 } from 'src/components/BaseQuestionnaireResponseForm/controls';
-import { FormWrapper, GroupItemComponent } from 'src/components/FormWrapper';
-import { saveFHIRResource, sdcService, service } from 'src/services';
+import {
+    groupControlComponents as readonlyGroupControlComponents,
+    itemComponents as readonlyItemComponents,
+    itemControlComponents as readonlyItemControlComponents,
+} from 'src/components/BaseQuestionnaireResponseForm/readonly-controls';
+import { FormWrapper, GroupItemComponent, ReadonlyFormWrapper } from 'src/components/FormWrapper';
+import { service } from 'src/services';
 
 export function QuestionnaireResponseForm(props: Pick<Props, 'questionnaireLoader'> & Partial<Props>) {
     return (
@@ -18,16 +24,54 @@ export function QuestionnaireResponseForm(props: Pick<Props, 'questionnaireLoade
             serviceProvider={
                 props.serviceProvider ?? {
                     service,
-                    saveFHIRResource,
                 }
             }
             fhirService={props.fhirService ?? service}
-            sdcService={props.sdcService ?? sdcService}
+            sdcServiceProvider={{
+                service: props.sdcServiceProvider?.service ?? props.fhirService ?? service,
+                assemble: props.sdcServiceProvider?.assemble ?? props.fhirService ?? service,
+                constraintCheck: props.sdcServiceProvider?.constraintCheck ?? props.fhirService ?? service,
+                populate: props.sdcServiceProvider?.populate ?? props.fhirService ?? service,
+                extract: props.sdcServiceProvider?.extract ?? props.fhirService ?? service,
+                saveQuestionnaireResponse:
+                    props.sdcServiceProvider?.saveQuestionnaireResponse ?? persistSaveQuestionnaireReponseService,
+            }}
             FormWrapper={props.FormWrapper ?? FormWrapper}
             groupItemComponent={props.groupItemComponent ?? GroupItemComponent}
             widgetsByQuestionType={props.widgetsByQuestionType ?? itemComponents}
             widgetsByQuestionItemControl={props.widgetsByQuestionItemControl ?? itemControlComponents}
             widgetsByGroupQuestionItemControl={props.widgetsByGroupQuestionItemControl ?? groupControlComponents}
+        />
+    );
+}
+
+export function ReadonlyQuestionnaireResponseForm(props: Pick<Props, 'questionnaireLoader'> & Partial<Props>) {
+    return (
+        <FHIRQuestionnaireResponseForm
+            {...props}
+            serviceProvider={
+                props.serviceProvider ?? {
+                    service,
+                }
+            }
+            fhirService={props.fhirService ?? service}
+            sdcServiceProvider={{
+                service: props.sdcServiceProvider?.service ?? props.fhirService ?? service,
+                assemble: props.sdcServiceProvider?.assemble ?? props.fhirService ?? service,
+                constraintCheck: props.sdcServiceProvider?.constraintCheck ?? props.fhirService ?? service,
+                populate: props.sdcServiceProvider?.populate ?? props.fhirService ?? service,
+                extract: props.sdcServiceProvider?.extract ?? props.fhirService ?? service,
+                saveQuestionnaireResponse:
+                    props.sdcServiceProvider?.saveQuestionnaireResponse ?? persistSaveQuestionnaireReponseService,
+            }}
+            FormWrapper={props.FormWrapper ?? ReadonlyFormWrapper}
+            groupItemComponent={props.groupItemComponent ?? GroupItemComponent}
+            widgetsByQuestionType={props.widgetsByQuestionType ?? readonlyItemComponents}
+            widgetsByQuestionItemControl={props.widgetsByQuestionItemControl ?? readonlyItemControlComponents}
+            widgetsByGroupQuestionItemControl={
+                props.widgetsByGroupQuestionItemControl ?? readonlyGroupControlComponents
+            }
+            readOnly={true}
         />
     );
 }
--- src/components/QuestionnaireResponseForm/hooks.ts
+++ src/components/QuestionnaireResponseForm/hooks.ts
@@ -9,17 +9,15 @@ import {
     itemControlComponents,
 } from 'src/components/BaseQuestionnaireResponseForm/controls';
 import { FormWrapper, GroupItemComponent } from 'src/components/FormWrapper';
-import { saveFHIRResource, sdcService, service } from 'src/services';
+import { service } from 'src/services';
 
 export function useQuestionnaireResponseForm(props: Pick<Props, 'questionnaireLoader'> & Partial<Props>) {
     return useFHIRQuestionnaireResponseForm({
         ...props,
         serviceProvider: props.serviceProvider ?? {
             service,
-            saveFHIRResource,
         },
         fhirService: props.fhirService ?? service,
-        sdcService: props.sdcService ?? sdcService,
         FormWrapper: props.FormWrapper ?? FormWrapper,
         groupItemComponent: props.groupItemComponent ?? GroupItemComponent,
         widgetsByQuestionType: props.widgetsByQuestionType ?? itemComponents,
