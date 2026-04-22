5d04f12c31edf6fccc6141bff8f966963bbbde08
Alex Pavlushkin <pavlushkin87@gmail.com>
2026-04-22 14:02:54 +0600

    Add HMB filter

--- src/containers/PatientList/utils.ts
+++ src/containers/PatientList/utils.ts
@@ -1,4 +1,44 @@
+import { t } from '@lingui/macro';
+
 import { SearchParams, formatFHIRDate } from '@beda.software/fhir-react';
+
+import { SearchBarColumn, SearchBarColumnType } from 'src/components/SearchBar/types';
+
+const HMB_SCREENING_QUESTIONNAIRE_ID = 'heavy-menstrual-bleeding-screening';
+
+function makeHmbAssessmentFilter(searchParam: string): SearchBarColumn {
+    return {
+        id: 'hmb-assessment',
+        searchParam,
+        type: SearchBarColumnType.CHOICE,
+        placeholder: t`HMB assessment`,
+        options: [
+            {
+                value: {
+                    Coding: {
+                        code: HMB_SCREENING_QUESTIONNAIRE_ID,
+                        display: t`Existing HMB assessment`,
+                    },
+                },
+            },
+        ],
+    };
+}
+
+export function makePatientListFilters(nameSearchParam: string, hmbSearchParam: string): SearchBarColumn[] {
+    return [
+        {
+            id: 'name',
+            searchParam: nameSearchParam,
+            type: SearchBarColumnType.SPLITSTRING,
+            placeholder: t`Find patient`,
+            placement: ['search-bar', 'table'],
+            searchBehavior: 'AND',
+            separator: ' ',
+        },
+        makeHmbAssessmentFilter(hmbSearchParam),
+    ];
+}
 
 export function getPatientSearchParamsForPractitioner(practitionerId: string): SearchParams {
     return {
