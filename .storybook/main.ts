83dec0d677347c5b4476a83813ae3d9929675fd0
Alex Lipovka <alex.lipovka@gmail.com>
2026-05-01 12:12:22 +0400

    upgrade storybook to 8.6.18

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
-        '@storybook/addon-styling',
+        '@storybook/addon-themes',
         '@chromatic-com/storybook',
     ],
 
