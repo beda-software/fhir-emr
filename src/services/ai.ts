e69032e07096424529e28a59ffc95c53feb029ce
Ilya Beda <ir4y.ix@gmail.com>
2026-05-18 12:28:16 +1000

    Use fhir service as basis fro aiservice

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -1,9 +1,10 @@
-import { service as aidboxService } from 'aidbox-react';
 import type { AxiosRequestConfig } from 'axios';
 
 import config from '@beda.software/emr-config';
 import { RemoteDataResult } from '@beda.software/remote-data';
 
+import { service } from 'src/services/fhir';
+
 export const aiService = async <S = any, F = any>(axiosConfig: AxiosRequestConfig): Promise<RemoteDataResult<S, F>> => {
-    return aidboxService({ ...axiosConfig, baseURL: config.aiAssistantServiceUrl });
+    return service({ ...axiosConfig, baseURL: config.aiAssistantServiceUrl });
 };
