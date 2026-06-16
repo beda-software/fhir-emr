db97cf9dcd56d65f09797df17787a9a8c8a796ac
Alex Lipovka <alex.lipovka@gmail.com>
2026-06-16 08:59:28 +0400

    ai assistant visibility depends on aiAssistantServiceUrl (#843)
    * ai assistant visibility depends on aiAssistantServiceUrl

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -6,5 +6,5 @@ import { RemoteDataResult } from '@beda.software/remote-data';
 import { service } from 'src/services/fhir';
 
 export const aiService = async <S = any, F = any>(axiosConfig: AxiosRequestConfig): Promise<RemoteDataResult<S, F>> => {
-    return service({ ...axiosConfig, baseURL: config.aiAssistantServiceUrl });
+    return service({ ...axiosConfig, baseURL: config.aiAssistantServiceUrl ?? undefined });
 };
