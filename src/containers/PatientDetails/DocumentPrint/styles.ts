8e3276bdaed373933e01cb987b2d460ca7735ee7
Alex Lipovka <alex.lipovka@gmail.com>
2026-06-18 02:14:13 +0400

    Printable mode (#851)
    Refactor DocumentPrint to use pagedjs and process custom headers, footers and cover

--- src/containers/PatientDetails/DocumentPrint/styles.ts
+++ src/containers/PatientDetails/DocumentPrint/styles.ts
@@ -4,28 +4,75 @@ import styled from 'styled-components';
 const { Text, Paragraph } = Typography;
 
 export const S = {
+    PrintWrapper: styled.div`
+        margin: 0 auto;
+        padding: 0;
+        @media print {
+            margin: 0 auto;
+        }
+
+        @media screen {
+            width: 210mm;
+        }
+    `,
     HeaderSpace: styled.div<{ $headerHeight?: string }>`
         height: 0;
         @media print {
             height: ${(props) => (props.$headerHeight ? props.$headerHeight : '10px')};
         }
     `,
-    FooterSpace: styled.div<{ $footerHeight?: string }>`
-        height: 0;
-        @media print {
-            height: ${(props) => (props.$footerHeight ? props.$footerHeight : '10px')};
+    Container: styled.div`
+        width: 100%;
+        max-width: 100%;
+        margin: 0;
+        padding: 0 30px;
+    `,
+    Cover: styled.div`
+        min-height: calc(var(--pagedjs-pagebox-height) - var(--pagedjs-margin-top) - var(--pagedjs-margin-bottom));
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        img {
+            max-width: 100%;
+            object-fit: contain;
         }
     `,
-    Container: styled.div<{ $pageMargin?: string }>`
-        width: 100%;
-        max-width: 767px;
-        margin: ${(props) => (props.$pageMargin ? props.$pageMargin : '40px auto')};
-        @page {
-            margin: 15mm 15mm 15mm 25mm;
+    Header: styled.div`
+        max-width: 100%;
+        img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: contain;
+        }
+    `,
+    HeaderFirstPage: styled.div`
+        img {
+            width: 100%;
+            height: auto;
+            object-fit: contain;
+        }
+    `,
+    FooterLastPage: styled.div`
+        img {
+            display: block;
+            width: 100%;
+            object-fit: contain;
+        }
+    `,
+    FooterLastPageWrapper: styled.div`
+        break-inside: avoid;
+    `,
+    Footer: styled.div`
+        max-width: 100%;
+        margin-top: auto;
+        img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: contain;
         }
         @media print {
-            max-width: 100%;
-            margin: 0 0 0 0;
+            display: none;
         }
     `,
     Title: styled(Text)`
