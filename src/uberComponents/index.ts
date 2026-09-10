591716027abe73796815487f058393c1b249eacb
Ilya Beda <ir4y.ix@gmail.com>
2026-09-10 17:52:38 +1000

    More exports

--- src/uberComponents/index.ts
+++ src/uberComponents/index.ts
@@ -1,4 +1,5 @@
 export * from './QuestionnaireModal';
 export * from './ResourceDetailPage';
 export * from './ResourceListPage';
+export * from './ResourceListPageContent';
 export * from './ViewChart';
